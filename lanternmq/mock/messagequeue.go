9676df6570a726975ec71bdb346926aac564e4b3
Lizzie Charbonneau <lizzie@mitre.org>
2019-10-28 12:49:01 -0400

    adds comments. changes 'target' to 'topic'. makes goroutine functions return errors on an error channel.

--- lanternmq/mock/messagequeue.go
+++ lanternmq/mock/messagequeue.go
@@ -2,12 +2,14 @@ package mock
 
 import (
 	"github.com/onc-healthit/lantern-back-end/lanternmq"
-	"github.com/streadway/amqp"
 )
 
 // Ensure MessageQueue implements lanternmq.MessageQueue.
 var _ lanternmq.MessageQueue = &MessageQueue{}
 
+// MessageQueue implements the lanternmq.MessageQueue interface and allows mock implementations of a MessageQueue.
+// Each MessageQueue method calls the corresponding method <methodName>Fn as assigned in the mock MessageQueue
+// structure. It also assigns <methodName>Invoked to true when <methodName> is called.
 type MessageQueue struct {
 	ConnectFn      func(username string, password string, host string, port string) error
 	ConnectInvoked bool
@@ -24,75 +26,86 @@ type MessageQueue struct {
 	PublishToQueueFn      func(chID lanternmq.ChannelID, qName string, message string) error
 	PublishToQueueInvoked bool
 
-	ConsumeFromQueueFn      func(chID lanternmq.ChannelID, qName string) (<-chan amqp.Delivery, error)
+	ConsumeFromQueueFn      func(chID lanternmq.ChannelID, qName string) (lanternmq.Messages, error)
 	ConsumeFromQueueInvoked bool
 
-	ProcessMessagesFn      func(msgs <-chan amqp.Delivery, handler lanternmq.MessageHandler, args *map[string]interface{}) error
+	ProcessMessagesFn      func(msgs lanternmq.Messages, handler lanternmq.MessageHandler, args *map[string]interface{}, errs chan<- error)
 	ProcessMessagesInvoked bool
 
-	DeclareTargetFn      func(chID lanternmq.ChannelID, name string) error
-	DeclareTargetInvoked bool
+	DeclareTopicFn      func(chID lanternmq.ChannelID, name string) error
+	DeclareTopicInvoked bool
 
-	PublishToTargetFn      func(chID lanternmq.ChannelID, name string, routingKey string, message string) error
-	PublishToTargetInvoked bool
+	PublishToTopicFn      func(chID lanternmq.ChannelID, name string, routingKey string, message string) error
+	PublishToTopicInvoked bool
 
-	DeclareTargetReceiveQueueFn      func(chID lanternmq.ChannelID, targetName string, qName string, routingKey string) error
-	DeclareTargetReceiveQueueInvoked bool
+	DeclareTopicReceiveQueueFn      func(chID lanternmq.ChannelID, topicName string, qName string, routingKey string) error
+	DeclareTopicReceiveQueueInvoked bool
 
 	CloseFn      func()
 	CloseInvoked bool
 }
 
+// Connect mocks lanternmq.Connect and sets mq.ConnectInvoked to true and calls mq.ConnectFn with the given arguments.
 func (mq *MessageQueue) Connect(username string, password string, host string, port string) error {
 	mq.ConnectInvoked = true
 	return mq.ConnectFn(username, password, host, port)
 }
 
+// CreateChannel mocks lanternmq.CreateChannel and sets mq.CreateChannelInvoked to true and calls mq.CreateChannelFn with the given arguments.
 func (mq *MessageQueue) CreateChannel() (lanternmq.ChannelID, error) {
 	mq.CreateChannelInvoked = true
-	return mq.CreateChannelFn(conn)
+	return mq.CreateChannelFn()
 }
 
+// NumConcurrentMsgs mocks lanternmq.NumConcurrentMsgs and sets mq.NumConcurrentMsgsInvoked to true and calls mq.NumConcurrentMsgsFn with the given arguments.
 func (mq *MessageQueue) NumConcurrentMsgs(chID lanternmq.ChannelID, num int) error {
 	mq.NumConcurrentMsgsInvoked = true
 	return mq.NumConcurrentMsgsFn(chID, num)
 }
 
+// DeclareQueue mocks lanternmq.DeclareQueue and sets mq.DeclareQueueInvoked to true and calls mq.DeclareQueueFn with the given arguments.
 func (mq *MessageQueue) DeclareQueue(chID lanternmq.ChannelID, name string) error {
 	mq.DeclareQueueInvoked = true
 	return mq.DeclareQueueFn(chID, name)
 }
 
+// PublishToQueue mocks lanternmq.PublishToQueue and sets mq.PublishToQueueInvoked to true and calls mq.PublishToQueueFn with the given arguments.
 func (mq *MessageQueue) PublishToQueue(chID lanternmq.ChannelID, qName string, message string) error {
 	mq.PublishToQueueInvoked = true
 	return mq.PublishToQueueFn(chID, qName, message)
 }
 
-func (mq *MessageQueue) ConsumeFromQueue(chID lanternmq.ChannelID, qName string) (<-chan amqp.Delivery, error) {
+// ConsumeFromQueue mocks lanternmq.ConsumeFromQueue and sets mq.ConsumeFromQueueInvoked to true and calls mq.ConsumeFromQueueFn with the given arguments.
+func (mq *MessageQueue) ConsumeFromQueue(chID lanternmq.ChannelID, qName string) (lanternmq.Messages, error) {
 	mq.ConsumeFromQueueInvoked = true
 	return mq.ConsumeFromQueueFn(chID, qName)
 }
 
-func (mq *MessageQueue) ProcessMessages(msgs <-chan amqp.Delivery, handler lanternmq.MessageHandler, args *map[string]interface{}) error {
+// ProcessMessages mocks lanternmq.ProcessMessages and sets mq.ProcessMessagesInvoked to true and calls mq.ProcessMessagesFn with the given arguments.
+func (mq *MessageQueue) ProcessMessages(msgs lanternmq.Messages, handler lanternmq.MessageHandler, args *map[string]interface{}, errs chan<- error) {
 	mq.ProcessMessagesInvoked = true
-	return mq.ProcessMessagesFn(msgs, handler, args)
+	mq.ProcessMessagesFn(msgs, handler, args, errs)
 }
 
-func (mq *MessageQueue) DeclareTarget(chID lanternmq.ChannelID, name string) error {
-	mq.DeclareTargetInvoked = true
-	return mq.DeclareTargetFn(chID, name)
+// DeclareTopic mocks lanternmq.DeclareTopic and sets mq.DeclareTopicInvoked to true and calls mq.DeclareTopicFn with the given arguments.
+func (mq *MessageQueue) DeclareTopic(chID lanternmq.ChannelID, name string) error {
+	mq.DeclareTopicInvoked = true
+	return mq.DeclareTopicFn(chID, name)
 }
 
-func (mq *MessageQueue) PublishToTarget(chID lanternmq.ChannelID, name string, routingKey string, message string) error {
-	mq.PublishToTargetInvoked = true
-	return mq.PublishToTargetFn(chID, name, routingKey, message)
+// PublishToTopic mocks lanternmq.PublishToTopic and sets mq.PublishToTopicInvoked to true and calls mq.PublishToTopicFn with the given arguments.
+func (mq *MessageQueue) PublishToTopic(chID lanternmq.ChannelID, name string, routingKey string, message string) error {
+	mq.PublishToTopicInvoked = true
+	return mq.PublishToTopicFn(chID, name, routingKey, message)
 }
 
-func (mq *MessageQueue) DeclareTargetReceiveQueue(chID lanternmq.ChannelID, targetName string, qName string, routingKey string) error {
-	mq.DeclareTargetReceiveQueueInvoked = true
-	return mq.DeclareTargetReceiveQueueFn(chID, targetName, qName, routingKey)
+// DeclareTopicReceiveQueue mocks lanternmq.DeclareTopicReceiveQueue and sets mq.DeclareTopicReceiveQueueInvoked to true and calls mq.DeclareTopicReceiveQueueFn with the given arguments.
+func (mq *MessageQueue) DeclareTopicReceiveQueue(chID lanternmq.ChannelID, topicName string, qName string, routingKey string) error {
+	mq.DeclareTopicReceiveQueueInvoked = true
+	return mq.DeclareTopicReceiveQueueFn(chID, topicName, qName, routingKey)
 }
 
+// Close mocks lanternmq.Close and sets mq.CloseInvoked to true and calls mq.CloseFn with the given arguments.
 func (mq *MessageQueue) Close() {
 	mq.CloseInvoked = true
 	mq.CloseFn()
--- lanternmq/rabbitmq/messagequeue.go
+++ lanternmq/rabbitmq/messagequeue.go
@@ -3,7 +3,6 @@ package rabbitmq
 import (
 	"errors"
 	"fmt"
-	"strconv"
 
 	"github.com/onc-healthit/lantern-back-end/lanternmq"
 	"github.com/streadway/amqp"
@@ -11,33 +10,47 @@ import (
 
 // Ensure MessageQueue implements lanternmq.MessageQueue.
 var _ lanternmq.MessageQueue = &MessageQueue{}
+
+// Ensure MessageMessages implements lanternmq.Messages.
 var _ lanternmq.Messages = &Messages{}
 
+// MessageQueue is a wrapper around RabbitMQ's implementation of queues and implements the lanternmq.MessageQueue
+// interface. It allows the user to:
+// * connect to a queueing service
+// * create a channel for that queueing service
+// * state how many messages a receiver can process at one time
+// * declare a durable queue, and send and receive from that queue
+// * declare a durable topic, and send and receive from that topic
+// * close the MessageQueue, which includes closing all channels and the connection to the underlying service.
 type MessageQueue struct {
 	connection *amqp.Connection
 	channels   []*amqp.Channel
 }
 
+// Messages wraps the delivery channel.
 type Messages struct {
 	deliveryChannel <-chan amqp.Delivery
 }
 
+// addChannel adds the given channel to the MessageQueue.channels array and returns the
+// index to that array casted to a lanternmq.ChannelID.
 func (mq *MessageQueue) addChannel(ch *amqp.Channel) (lanternmq.ChannelID, error) {
 	if mq.channels == nil {
 		mq.channels = []*amqp.Channel{}
 	}
 	mq.channels = append(mq.channels, ch)
 	index := len(mq.channels) - 1
-	id := lanternmq.ChannelID(strconv.Itoa(index))
+	id := lanternmq.ChannelID(index)
 
 	return id, nil
 }
 
+// getChannel retrieves the channel provided by `id` by casting `id` back to an integer and
+// retrieving the channel at the corresponding index of MessageQueue.channels array.
 func (mq *MessageQueue) getChannel(id lanternmq.ChannelID) (*amqp.Channel, error) {
-	idStr := string(id)
-	idInt, err := strconv.Atoi(idStr)
-	if err != nil {
-		return nil, err
+	idInt, ok := id.(int)
+	if !ok {
+		return nil, errors.New("ChannelID not of correct type")
 	}
 	if idInt >= len(mq.channels) {
 		return nil, errors.New("no channel with the requested ID was found")
@@ -46,6 +59,7 @@ func (mq *MessageQueue) getChannel(id lanternmq.ChannelID) (*amqp.Channel, error
 	return ch, nil
 }
 
+// Connect creates a connection to a RabbitMQ service.
 func (mq *MessageQueue) Connect(username string, password string, host string, port string) error {
 	s := fmt.Sprintf("amqp://%s:%s@%s:%s/", username, password, host, port)
 	conn, err := amqp.Dial(s)
@@ -57,6 +71,9 @@ func (mq *MessageQueue) Connect(username string, password string, host string, p
 	return err
 }
 
+// CreateChannel creates a channel to the RabbitMQ service that has already been connected to.
+// If the RabbitMQ service has not been connected to already, an error is thrown.
+// The channel's ID is returned.
 func (mq *MessageQueue) CreateChannel() (lanternmq.ChannelID, error) {
 	var err error
 	if mq.connection == nil {
@@ -73,6 +90,7 @@ func (mq *MessageQueue) CreateChannel() (lanternmq.ChannelID, error) {
 	return id, err
 }
 
+// NumConcurrentMsgs defines how many messages the user can process from the channel at one time.
 func (mq *MessageQueue) NumConcurrentMsgs(chID lanternmq.ChannelID, num int) error {
 	ch, err := mq.getChannel(chID)
 	if err != nil {
@@ -91,14 +109,22 @@ func (mq *MessageQueue) NumConcurrentMsgs(chID lanternmq.ChannelID, num int) err
 	return err
 }
 
-func (mq *MessageQueue) DeclareQueue(chID lanternmq.ChannelID, name string) error {
+// DeclareQueue creates a queue with the given name on the given channel using RabbitMQ's
+// QueueDeclare method with the following arguments:
+// * name: qName
+// * durable: true
+// * autoDelete: false
+// * exclusive: false
+// * noWait: false
+// * args: nil
+func (mq *MessageQueue) DeclareQueue(chID lanternmq.ChannelID, qName string) error {
 	ch, err := mq.getChannel(chID)
 	if err != nil {
 		return err
 	}
 
 	_, err = ch.QueueDeclare(
-		name,  // name
+		qName, // name
 		true,  // durable
 		false, // delete when unused
 		false, // exclusive
@@ -112,6 +138,16 @@ func (mq *MessageQueue) DeclareQueue(chID lanternmq.ChannelID, name string) erro
 	return err
 }
 
+// PublishToQueue publishes 'message' on the queue with name 'qName' over the channenl with ID 'chID'
+// by calling the RabbitMQ Publish method with the following arguments:
+// exchange: ""
+// key: qName
+// mandatory: false
+// immediate: false
+// publishing:
+//   DeliveryMode: amqp.Persistent
+//   ContentType: "text/plain"
+//   Body: []byte(message)
 func (mq *MessageQueue) PublishToQueue(chID lanternmq.ChannelID, qName string, message string) error {
 	ch, err := mq.getChannel(chID)
 	if err != nil {
@@ -131,6 +167,16 @@ func (mq *MessageQueue) PublishToQueue(chID lanternmq.ChannelID, qName string, m
 	return err
 }
 
+// ConsumeFromQueue opens a receive channel for amqp.Delivery objects for the queue with name 'qName'
+// over the channel with ID 'chID'. ConsumeFromQueue wraps amqp.Delivery in a lanternmq.Messages object.
+// ConsumeFromQueue creates the receive channel using the RabbitMQ Consume method with the following arguments:
+// queue: qName
+// consumer: ""
+// autoAck: false
+// exclusive: false
+// noLocal: false
+// noWait: false
+// args: nil
 func (mq *MessageQueue) ConsumeFromQueue(chID lanternmq.ChannelID, qName string) (lanternmq.Messages, error) {
 	ch, err := mq.getChannel(chID)
 	if err != nil {
@@ -151,23 +197,40 @@ func (mq *MessageQueue) ConsumeFromQueue(chID lanternmq.ChannelID, qName string)
 	return &msgs, err
 }
 
-func (mq *MessageQueue) ProcessMessages(msgs lanternmq.Messages, handler lanternmq.MessageHandler, args *map[string]interface{}) error {
-	msgsd, converted := msgs.(*Messages)
-	if !converted {
-		return errors.New("the messages are of the wrong type")
+// ProcessMessages takes 'msgs', which wraps a receive channel for amqp.Delivery objects, and processes each Delivery
+// object by retrieving the message from the Delivery object and providing that along with 'args' to the
+// lanternmq.MessageHandler 'handler'. An acknowledgement is sent to the sender after each message is processed.
+// If there's an error processing a message, the error is sent to the 'errs' channel.
+// ProcessMessages should be called as a gorouting. Example:
+//     go mq.ProcessMessages(msgs, handler, nil, errs)
+func (mq *MessageQueue) ProcessMessages(msgs lanternmq.Messages, handler lanternmq.MessageHandler, args *map[string]interface{}, errs chan<- error) {
+	msgsd, ok := msgs.(*Messages)
+	if !ok {
+		errs <- errors.New("the messages are of the wrong type")
 	}
 
 	for d := range msgsd.deliveryChannel {
 		err := handler(d.Body, args)
 		if err != nil {
-			return err
+			errs <- err
 		}
-		d.Ack(false)
+		err = d.Ack(false)
+		if err != nil {
+			errs <- err
+		}
 	}
-	return nil
 }
 
-func (mq *MessageQueue) DeclareTarget(chID lanternmq.ChannelID, name string) error {
+// DeclareTopic creates a target named 'name' over the channel with ID 'chID'. It uses RabbitMQ's
+// ExchangeDeclare method with the following arguments:
+// name: name
+// kind: "topic"
+// durable: true
+// autoDelete: false
+// intenral: false
+// noWait: false
+// args: nil
+func (mq *MessageQueue) DeclareTopic(chID lanternmq.ChannelID, name string) error {
 	ch, err := mq.getChannel(chID)
 	if err != nil {
 		return err
@@ -189,7 +252,16 @@ func (mq *MessageQueue) DeclareTarget(chID lanternmq.ChannelID, name string) err
 	return err
 }
 
-func (mq *MessageQueue) PublishToTarget(chID lanternmq.ChannelID, name string, routingKey string, message string) error {
+// PublishToTopic sends 'message' to the topic 'name' over the channel with ID 'chID' with routing key 'routingKey'. It
+// uses RabbitMQ's Publish method with the following arguments:
+// exchange: name
+// key: routingKey
+// mandatory: false
+// immediate: false
+// publishing:
+//   ContentType: "text/plain"
+//   Body: []byte(message)
+func (mq *MessageQueue) PublishToTopic(chID lanternmq.ChannelID, name string, routingKey string, message string) error {
 	ch, err := mq.getChannel(chID)
 	if err != nil {
 		return err
@@ -211,7 +283,23 @@ func (mq *MessageQueue) PublishToTarget(chID lanternmq.ChannelID, name string, r
 	return err
 }
 
-func (mq *MessageQueue) DeclareTargetReceiveQueue(chID lanternmq.ChannelID, targetName string, qName string, routingKey string) error {
+// DeclareTopicReceiveQueue creates a queue named 'qName' to receive messages from the topic named 'topicName'
+// with routing key 'routingKey' over the channel with ID 'chID'. It uses the RabbitMQ method QueueDeclare with
+// the following arguments:
+// name: qName
+// durable: false
+// autoDelete: false
+// exclusive: true
+// noWait: false
+// args: nil
+//
+// It then calls the RabbitMQ method QueueBind with the following arguments:
+// name: qName
+// key: routingKey
+// exchange: topicName
+// noWait: false
+// args: nil
+func (mq *MessageQueue) DeclareTopicReceiveQueue(chID lanternmq.ChannelID, topicName string, qName string, routingKey string) error {
 	ch, err := mq.getChannel(chID)
 	if err != nil {
 		return err
@@ -231,19 +319,21 @@ func (mq *MessageQueue) DeclareTargetReceiveQueue(chID lanternmq.ChannelID, targ
 	}
 
 	err = ch.QueueBind(
-		qName,        // queue name
-		routingKey,   // routing key
-		"logs_topic", // exchange
+		qName,      // queue name
+		routingKey, // routing key
+		topicName,  // exchange
 		false,
 		nil)
 	if err != nil {
-		err = fmt.Errorf("unable to bind queue %s to target %s with routing key %s", qName, targetName, routingKey)
+		err = fmt.Errorf("unable to bind queue %s to target %s with routing key %s", qName, topicName, routingKey)
 		return err
 	}
 
 	return err
 }
 
+// Close closes each channel that's been created, and then closes the connection to the underlying RabbitMQ
+// message service.
 func (mq *MessageQueue) Close() {
 	if mq.channels != nil {
 		for _, ch := range mq.channels {
