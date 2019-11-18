11d3f4903b4129d8ee1fe159770813bdd500dcdd
Matt Mayer <mmayer@mitre.org>
2019-11-18 09:38:18 -0500

    Publish more service ports for testing
    Add end to end test infastructure
    
    Add e2e instructions to readme
    
    Add certs directory to gitignore
    
    Add certs volume to endpoint querier
    
    Add nil check for TLS version
    
    Reference integration container by container name
    
    Add assertions for metrics info in test
    
    Add instructions to README about running behind SSL-Inspecting proxy
    
    Update e2e test command and README index
    
    Move test server setup to TestMain funciton
    
    add prometheus test
    
    Make tests return non-0 status code on failure
    
    Add vendor file to e2e dir
    
    Remove useless yarn.lock
    
    Refactor dockerfile
    
    Add prometheus and database tests to integration test
    
    Update go.sum
    
    Rename integration test
    
    Address gofmt issues
    
    Adjust http write failure in test to not quit out
    
    Add integration tag to integration test
    
    Run integration tests in travis
    
    Attempt travis build without docker compose override
    
    Add failure restart policy to prometheus_postgresql_adapter
    
    Update env variables used by travis
    
    fix typo in env variable
    
    Add postgres env variables
    
    Adjust env username and password
    
    Adjust env username and password
    
    Make wait-for-it exit with status 1 when no response
    
    Add source attribution
    
    Address PR comments in README
    
    Merge README contents with master

---
+++ e2e/vendor/github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/endpointmanager/fhirendpoint.go
@@ -0,0 +1,63 @@
+package endpointmanager
+
+import (
+	"time"
+)
+
+// FHIREndpoint represents a fielded FHIR API endpoint hosted by a
+// HealthITProduct and populated by a ProviderOrganization.
+// Information about the FHIR API endpoint is populated by the FHIR
+// capability statement found at that endpoint as well as information
+// discovered about the IP address of the endpoint.
+type FHIREndpoint struct {
+	ID                    int
+	URL                   string
+	FHIRVersion           string
+	AuthorizationStandard string               // examples: OAuth 2.0, Basic, etc.
+	Location              *Location            // location of the FHIR API endpoint's IP address from ipstack.com.
+	CapabilityStatement   *CapabilityStatement // the JSON representation of the FHIR capability statement
+	CreatedAt             time.Time
+	UpdatedAt             time.Time
+}
+
+// FHIREndpointStore is the interface for interacting with the storage layer that holds
+// FHIR endpoint objects.
+type FHIREndpointStore interface {
+	GetFHIREndpoint(int) (*FHIREndpoint, error)
+	GetFHIREndpointUsingURL(string) (*FHIREndpoint, error)
+
+	AddFHIREndpoint(*FHIREndpoint) error
+	UpdateFHIREndpoint(*FHIREndpoint) error
+	DeleteFHIREndpoint(*FHIREndpoint) error
+
+	Close()
+}
+
+// Equal checks each field of the two FHIREndpoints except for the database ID, CreatedAt and UpdatedAt fields to see if they are equal.
+func (e *FHIREndpoint) Equal(e2 *FHIREndpoint) bool {
+	if e == nil && e2 == nil {
+		return true
+	} else if e == nil {
+		return false
+	} else if e2 == nil {
+		return false
+	}
+
+	if e.URL != e2.URL {
+		return false
+	}
+	if e.FHIRVersion != e2.FHIRVersion {
+		return false
+	}
+	if e.AuthorizationStandard != e2.AuthorizationStandard {
+		return false
+	}
+	if !e.Location.Equal(e2.Location) {
+		return false
+	}
+	if !e.CapabilityStatement.Equal(e2.CapabilityStatement) {
+		return false
+	}
+
+	return true
+}
