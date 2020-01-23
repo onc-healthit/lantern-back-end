e1cd57fa72b307c944f760e4d3094a3c8e470a32
Lizzie Charbonneau <lizzie@mitre.org>
2020-01-22 20:42:00 -0500

    added ability to find vendor name

---
+++ endpointmanager/pkg/chplmapper/capabilityinfo.go
@@ -0,0 +1,95 @@
+package chplmapper
+
+import (
+	"context"
+	"strings"
+
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/capabilityparser"
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/endpointmanager"
+	"github.com/pkg/errors"
+)
+
+var fluffWords = []string{
+	"inc.",
+	"inc",
+	"llc",
+	"corp.",
+	"corp",
+	"corporation",
+	"lmt",
+	"lmt.",
+	"limited",
+	"corporation",
+}
+
+// TODO: should this throw an error if there's no publisher in the capability statement?
+func getVendorMatch(ctx context.Context, capStat capabilityparser.CapabilityStatement, store endpointmanager.HealthITProductStore) (string, error) {
+	var vendorsNorm []string
+
+	publisher, err := capStat.GetPublisher()
+	if err != nil {
+		return "", errors.Wrap(err, "unable to get vendor information from capability statement")
+	}
+	publisherNorm := normalizeName(publisher)
+
+	vendorsRaw, err := store.GetDevelopers(ctx)
+	if err != nil {
+		return "", errors.Wrap(err, "error retrieving vendor list from database")
+	}
+	for _, vendorRaw := range vendorsRaw {
+		vendorNorm := normalizeName(vendorRaw)
+		vendorsNorm = append(vendorsNorm, vendorNorm)
+	}
+
+	match, err := matchName(publisherNorm, vendorsNorm, vendorsRaw)
+	if err != nil {
+		return "", errors.Wrap(err, "error matching capability statement publisher to health it developers in database")
+	}
+
+	return match, nil
+}
+
+func matchName(name string, vendorsNorm []string, vendorsRaw []string) (string, error) {
+	// exact match
+	for i, vendor := range vendorsNorm {
+		if name == vendor {
+			return vendorsRaw[i], nil
+		}
+	}
+
+	// substring match
+	var matches []int
+	for i, vendor := range vendorsNorm {
+		if strings.Contains(vendor, name) {
+			matches = append(matches, i)
+		}
+	}
+	for i, vendor := range vendorsNorm {
+		if strings.Contains(name, vendor) {
+			matches = append(matches, i)
+		}
+	}
+	if len(matches) == 1 {
+		return vendorsRaw[matches[0]], nil
+	}
+
+	// fuzzy match
+	// TODO
+
+	return "", nil
+}
+
+func normalizeName(name string) string {
+	name = strings.ToLower(name)
+
+	for _, fluff := range fluffWords {
+		if strings.HasSuffix(name, fluff) {
+			index := strings.LastIndex(name, fluff)
+			name = name[:index]
+			break
+		}
+	}
+
+	name = strings.TrimRight(name, ",. ")
+	return name
+}
---
+++ endpointmanager/pkg/chplmapper/capabilityinfo_test.go
@@ -0,0 +1,105 @@
+package chplmapper
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/capabilityparser"
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/endpointmanager"
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/endpointmanager/mock"
+	th "github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/testhelper"
+)
+
+func Test_getVendorMatch(t *testing.T) {
+	var path string
+	var err error
+	var expected string
+
+	var dstu2JSON []byte
+	var dstu2 capabilityparser.CapabilityStatement
+	var vendor string
+
+	ctx := context.Background()
+	store, err := getMockStore()
+	th.Assert(t, err == nil, err)
+
+	// cerner
+	expected = "Cerner Corporation"
+
+	path = filepath.Join("testdata", "cerner_capability_dstu2.json")
+	dstu2JSON, err = ioutil.ReadFile(path)
+	th.Assert(t, err == nil, err)
+
+	dstu2, err = capabilityparser.NewCapabilityStatement(dstu2JSON)
+	th.Assert(t, err == nil, err)
+
+	vendor, err = getVendorMatch(ctx, dstu2, store)
+	th.Assert(t, err == nil, err)
+	th.Assert(t, vendor == expected, fmt.Sprintf("expected vendor to be %s. Got %s.", expected, vendor))
+
+	// epic
+	expected = "" // the capability statement is missing the publisher
+
+	path = filepath.Join("testdata", "epic_capability_dstu2.json")
+	dstu2JSON, err = ioutil.ReadFile(path)
+	th.Assert(t, err == nil, err)
+
+	dstu2, err = capabilityparser.NewCapabilityStatement(dstu2JSON)
+	th.Assert(t, err == nil, err)
+
+	vendor, err = getVendorMatch(ctx, dstu2, store)
+	th.Assert(t, err == nil, err)
+	th.Assert(t, vendor == expected, fmt.Sprintf("expected vendor to be %s. Got %s.", expected, vendor))
+
+	// allscripts
+	expected = "Allscripts" // the capability statement is missing the publisher
+
+	path = filepath.Join("testdata", "allscripts_capability_dstu2.json")
+	dstu2JSON, err = ioutil.ReadFile(path)
+	th.Assert(t, err == nil, err)
+
+	dstu2, err = capabilityparser.NewCapabilityStatement(dstu2JSON)
+	th.Assert(t, err == nil, err)
+
+	vendor, err = getVendorMatch(ctx, dstu2, store)
+	th.Assert(t, err == nil, err)
+	th.Assert(t, vendor == expected, fmt.Sprintf("expected vendor to be %s. Got %s.", expected, vendor))
+
+	// meditech
+	expected = "Medical Information Technology, Inc. (MEDITECH)" // the capability statement is missing the publisher
+
+	path = filepath.Join("testdata", "meditech_capability_dstu2.json")
+	dstu2JSON, err = ioutil.ReadFile(path)
+	th.Assert(t, err == nil, err)
+
+	dstu2, err = capabilityparser.NewCapabilityStatement(dstu2JSON)
+	th.Assert(t, err == nil, err)
+
+	vendor, err = getVendorMatch(ctx, dstu2, store)
+	th.Assert(t, err == nil, err)
+	th.Assert(t, vendor == expected, fmt.Sprintf("expected vendor to be %s. Got %s.", expected, vendor))
+}
+
+func getMockStore() (endpointmanager.HealthITProductStore, error) {
+	hitp, err := mock.NewStore()
+	if err != nil {
+		return nil, err
+	}
+
+	hitp.GetDevelopersFn = func(ctx context.Context) ([]string, error) {
+		devList := []string{
+			"Epic Systems Corporation",
+			"Cerner Corporation",
+			"Cerner Health Services, Inc.",
+			"Medical Information Technology, Inc. (MEDITECH)",
+			"Allscripts",
+		}
+
+		return devList, nil
+	}
+
+	return hitp, nil
+}
