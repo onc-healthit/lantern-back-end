1b7de2fff6897944105559018a335710352da92d
Matt Mayer <mmayer@mitre.org>
2020-02-26 09:22:52 -0500

    Local vendor updates

--- endpointmanager/vendor/github.com/onc-healthit/lantern-back-end/endpointmanager/cmd/endpointlinker/main.go
+++ endpointmanager/vendor/github.com/onc-healthit/lantern-back-end/endpointmanager/cmd/endpointlinker/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"strconv"
 	"context"
 	"os"
 	"log"
@@ -17,12 +16,6 @@ func failOnError(errString string, err error) {
 	}
 }
 
-func verbosePrint(message string, verbose bool) {
-	if verbose == true {
-		println(message)
-	}
-}
-
 func main() {
 	var verbose = false
 	if len(os.Args) > 1 && os.Args[1] == "--verbose" {
@@ -30,44 +23,14 @@ func main() {
 	}
 
 	err := config.SetupConfig()
-	failOnError("Error setting up confit", err)
+	failOnError("Error setting up config", err)
 	ctx := context.Background()
 
 	store, err := postgresql.NewStore(viper.GetString("dbhost"), viper.GetInt("dbport"), viper.GetString("dbuser"), viper.GetString("dbpassword"), viper.GetString("dbname"), viper.GetString("dbsslmode"))
 	failOnError("Error creating store", err)
 
-	fhirEndpointOrgNames, err := store.GetAllOrgNames(ctx)
-	failOnError("Error getting endpoint org names", err)
+	err = endpointlinker.LinkAllOrgsAndEndpoints(ctx, store, verbose);
+	failOnError("Error linking all orgs and enpoints", err)
 
-	npiOrgNames, err := store.GetAllNormalizedOrgNames(ctx)
-	if err != nil {
-		failOnError("Error getting normalized org names", err)
-	}
-
-	matchCount := 0
-	unmatchable := []string{}
-	// Iterate through fhir endpoints
-	for _, endpoint := range fhirEndpointOrgNames {
-		normalizedEndpointName := endpointlinker.NormalizeOrgName(endpoint.OrganizationName)
-		matches := []int{}
-		matches, err = endpointlinker.GetIdsOfMatchingNPIOrgs(npiOrgNames, normalizedEndpointName, verbose)
-		if (len(matches) > 0){
-			matchCount += 1
-			// Iterate over matches and add to linking table
-			for _, match := range matches {
-				store.LinkOrganizationToEndpoint(ctx, match, endpoint.ID)
-			}
-		}else{
-			unmatchable = append(unmatchable, endpoint.OrganizationName )
-		}
-
-	}
-
-	verbosePrint("Match Total: " + strconv.Itoa(matchCount) + "/" + strconv.Itoa(len(fhirEndpointOrgNames)), verbose)
-
-	verbosePrint("UNMATCHABLE ENDPOINT ORG NAMES", verbose)
-	for _, name := range unmatchable {
-		verbosePrint(name, verbose)
-	}
 
 }
