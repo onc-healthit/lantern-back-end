fa8e353db92cd2264dbf74ef19835aae5eea8b9c
Matt Mayer <mmayer@mitre.org>
2020-03-04 16:38:37 -0500

    Improve test assertions

--- endpointmanager/pkg/endpointlinker/endpointlinker_test.go
+++ endpointmanager/pkg/endpointlinker/endpointlinker_test.go
@@ -3,64 +3,51 @@ package endpointlinker
 import (
 	"testing"
 	"strconv"
+	th "github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/testhelper"
+
 )
 
 func Test_NormalizeOrgName(t *testing.T) {
 	orgName := "AMBULANCE & and-chair. SERVICE!"
 	expected := "AMBULANCE  AND CHAIR SERVICE"
 	normalized := NormalizeOrgName(orgName)
-	if normalized != expected {
-		t.Errorf("Organization name normalization failed. Expected: "+ expected + " Got: " + normalized)
-	}
+	th.Assert(t, (normalized == expected), "Organization name normalization failed. Expected: "+ expected + " Got: " + normalized)
 }
 
 func Test_CalculateJaccardIndex(t *testing.T) {
 	jacccardIndex := CalculateJaccardIndex("FOO BAR", "FOO BAR")
-	if jacccardIndex != 1 {
-		ind := strconv.FormatFloat(jacccardIndex, 'f', -1, 64)
-		t.Errorf("Jacard index expected to be 1, was " + ind)
-	}
+	ind := strconv.FormatFloat(jacccardIndex, 'f', -1, 64)
+	th.Assert(t, (jacccardIndex == 1), "Jacard index expected to be 1, was " + ind)
 
 	jacccardIndex = CalculateJaccardIndex("FOO BAZ BAR", "FOO BAR")
-	if jacccardIndex != .6666666666666666 {
-		ind := strconv.FormatFloat(jacccardIndex, 'f', -1, 64)
-		t.Errorf("Jacard index expected to be 1, was " + ind)
-	}
+	ind = strconv.FormatFloat(jacccardIndex, 'f', -1, 64)
+	th.Assert(t, (jacccardIndex == .6666666666666666), "Jacard index expected to be .6666666666666666, was " + ind)
+
+	jacccardIndex = CalculateJaccardIndex("FOO FOO BAR", "FOO BAR")
+	ind = strconv.FormatFloat(jacccardIndex, 'f', -1, 64)
+	th.Assert(t, (jacccardIndex == .6666666666666666), "Jacard index expected to be .6666666666666666, was " + ind)
 }
 
 func Test_IntersectionCount(t *testing.T) {
 	emptyListIntersections := intersectionCount([]string{},[]string{})
-	if emptyListIntersections != 0 {
-		t.Errorf("Intersection count of empty lists should be zero, got " + strconv.Itoa(emptyListIntersections))
-	}
+	th.Assert(t, (emptyListIntersections == 0), "Intersection count of empty lists should be zero, got " + strconv.Itoa(emptyListIntersections))
 
 	emptyListIntersections = intersectionCount([]string{"foo"},[]string{})
-	if emptyListIntersections != 0 {
-		t.Errorf("Intersection count of empty lists should be zero, got " + strconv.Itoa(emptyListIntersections))
-	}
+	th.Assert(t, (emptyListIntersections == 0), "Intersection count of empty lists should be zero, got " + strconv.Itoa(emptyListIntersections))
 
 	nonEmptyListIntersections := intersectionCount([]string{"foo"},[]string{"bar"})
-	if nonEmptyListIntersections != 0 {
-		t.Errorf("Intersection count of empty lists should be zero, got " + strconv.Itoa(nonEmptyListIntersections))
-	}
+	th.Assert(t, (emptyListIntersections == 0), "Intersection count of empty lists should be zero, got " + strconv.Itoa(emptyListIntersections))
 
 	nonEmptyListIntersections = intersectionCount([]string{"foo"},[]string{"foo"})
-	if nonEmptyListIntersections != 1 {
-		t.Errorf("Intersection count of empty lists should be zero, got " + strconv.Itoa(nonEmptyListIntersections))
-	}
+	th.Assert(t, (nonEmptyListIntersections == 1), "Intersection count of empty lists should be one, got " + strconv.Itoa(nonEmptyListIntersections))
 
 	nonEmptyListIntersections = intersectionCount([]string{"foo","bar"},[]string{"bar"})
-	if nonEmptyListIntersections != 1 {
-		t.Errorf("Intersection count of empty lists should be zero, got " + strconv.Itoa(nonEmptyListIntersections))
-	}
+	th.Assert(t, (nonEmptyListIntersections == 1), "Intersection count of empty lists should be one, got " + strconv.Itoa(nonEmptyListIntersections))
 
 	nonEmptyListIntersections = intersectionCount([]string{"foo","bar"},[]string{"bar", "foo"})
-	if nonEmptyListIntersections != 2 {
-		t.Errorf("Intersection count of empty lists should be zero, got " + strconv.Itoa(nonEmptyListIntersections))
-	}
+	th.Assert(t, (nonEmptyListIntersections == 2), "Intersection count of empty lists should be two, got " + strconv.Itoa(nonEmptyListIntersections))
 
 	nonEmptyListIntersections = intersectionCount([]string{"foo","bar","foo","foo"},[]string{"bar", "foo", "foo"})
-	if nonEmptyListIntersections != 3 {
-		t.Errorf("Intersection count of empty lists should be zero, got " + strconv.Itoa(nonEmptyListIntersections))
-	}
+	th.Assert(t, (nonEmptyListIntersections == 3), "Intersection count of empty lists should be three, got " + strconv.Itoa(nonEmptyListIntersections))
+
 }
