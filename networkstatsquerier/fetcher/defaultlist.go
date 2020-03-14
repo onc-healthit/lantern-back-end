bd69b62ec18726cf02deed3ddc96eee04b46767c
Brianna Mathiowetz <bmathiowetz@mitre.org>
2020-03-14 19:03:09 -0400

    Update default list to only handle the default enpoint list format. Add an epic list handler.

--- networkstatsquerier/fetcher/defaultlist.go
+++ networkstatsquerier/fetcher/defaultlist.go
@@ -1,17 +1,12 @@
 package fetcher
 
-// DefaultList implements the Endpoints interface for endpoint lists in the default format
-// which is the format that matches the ListOfEndpoints struct
-type DefaultList struct{}
-
-// GetEndpoints takes the list of cerner endpoints and formats it into a ListOfEndpoints
-func (dl DefaultList) GetEndpoints(defaultList []map[string]interface{}) (ListOfEndpoints, error) {
+// GetEndpoints takes the a list of endpoints and formats it into a ListOfEndpoints
+func getDefaultEndpoints(defaultList []map[string]interface{}, source string) ListOfEndpoints {
 	var finalList ListOfEndpoints
 	var innerList []EndpointEntry
-
 	for entry := range defaultList {
 		fhirEntry := EndpointEntry{
-			ListSource: "https://open.epic.com/MyApps/EndpointsJson",
+			ListSource: source,
 		}
 		orgName, orgOk := defaultList[entry]["OrganizationName"].(string)
 		if orgOk {
@@ -25,5 +20,5 @@ func (dl DefaultList) GetEndpoints(defaultList []map[string]interface{}) (ListOf
 	}
 
 	finalList.Entries = innerList
-	return finalList, nil
+	return finalList
 }
