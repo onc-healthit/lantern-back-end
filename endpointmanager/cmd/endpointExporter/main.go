a24df5b4e6e02f2a7572cf7f19206f878fba3c33
Matt Mayer <mmayer@mitre.org>
2020-04-01 09:55:58 -0400

    Add csv export and make commmand for csv export

---
+++ endpointmanager/cmd/endpointExporter/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/config"
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/endpointmanager/postgresql"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func failOnError(errString string, err error) {
+	if err != nil {
+		log.Fatalf("%s %s", errString, err)
+	}
+}
+
+func main() {
+	log.Info("Starting to export endpoints")
+
+	err := config.SetupConfig()
+	failOnError("Error setting up config", err)
+	ctx := context.Background()
+
+	store, err := postgresql.NewStore(viper.GetString("dbhost"), viper.GetInt("dbport"), viper.GetString("dbuser"), viper.GetString("dbpassword"), viper.GetString("dbname"), viper.GetString("dbsslmode"))
+	failOnError("Error creating store", err)
+
+	sql_query := "COPY (SELECT * FROM org_mapping) TO '/tmp/export.csv' DELIMITER ',' CSV HEADER;"
+	_, err = store.DB.ExecContext(ctx,sql_query)
+	failOnError("Error exporting csv", err)
+
+}
