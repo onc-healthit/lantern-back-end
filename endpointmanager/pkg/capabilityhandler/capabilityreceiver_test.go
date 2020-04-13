6be3533dceddd16beb1e69797ce5706f67439226
Lizzie Charbonneau <lizzie@mitre.org>
2020-04-13 09:46:37 -0400

    redid capabilityreceiver tests

--- endpointmanager/pkg/capabilityhandler/capabilityreceiver_test.go
+++ endpointmanager/pkg/capabilityhandler/capabilityreceiver_test.go
@@ -37,7 +37,7 @@ func convertInterfaceToBytes(message map[string]interface{}) ([]byte, error) {
 	return returnMsg, nil
 }
 
-func setup(t *testing.T) {
+func setupCapabilityStatement(t *testing.T) {
 	// capability statement
 	path := filepath.Join("../testdata", "cerner_capability_dstu2.json")
 	csJSON, err := ioutil.ReadFile(path)
@@ -52,7 +52,7 @@ func setup(t *testing.T) {
 }
 
 func Test_formatMessage(t *testing.T) {
-	setup(t)
+	setupCapabilityStatement(t)
 	expectedEndpt := testFhirEndpoint
 	tmpMessage := testQueueMsg
 
@@ -112,71 +112,3 @@ func Test_formatMessage(t *testing.T) {
 	th.Assert(t, returnErr != nil, "Expected an error to be thrown due to an incorrect HTTP response")
 	tmpMessage["httpResponse"] = 200
 }
-
-// func Test_saveMsgInDB(t *testing.T) {
-// 	setup(t)
-// 	store := mock.NewBasicMockFhirEndpointStore()
-// 	hitpStore := mock.NewBasicMockHealthITProductStore()
-
-// 	args := make(map[string]interface{})
-// 	args["epStore"] = store
-// 	args["hitpStore"] = hitpStore
-
-// 	expectedEndpt := testFhirEndpoint
-// 	expectedEndpt.Vendor = "Cerner Corporation"
-// 	queueTmp := testQueueMsg
-
-// 	queueMsg, err := convertInterfaceToBytes(queueTmp)
-// 	th.Assert(t, err == nil, err)
-
-// 	// check that nothing is stored and that saveMsgInDB throws an error if the context is canceled
-// 	testCtx, cancel := context.WithCancel(context.Background())
-// 	args["ctx"] = testCtx
-// 	cancel()
-// 	err = saveMsgInDB(queueMsg, &args)
-// 	th.Assert(t, len(store.(*mock.BasicMockStore).FhirEndpointData) == 0, "should not have stored data")
-// 	th.Assert(t, errors.Cause(err) == context.Canceled, "should have errored out with root cause that the context was canceled")
-
-// 	// reset context
-// 	args["ctx"] = context.Background()
-
-// 	// check that new item is stored
-// 	err = saveMsgInDB(queueMsg, &args)
-// 	th.Assert(t, err == nil, err)
-// 	th.Assert(t, len(store.(*mock.BasicMockStore).FhirEndpointData) == 1, "did not store data as expected")
-// 	th.Assert(t, expectedEndpt.Equal(store.(*mock.BasicMockStore).FhirEndpointData[0]), "stored data does not equal expected store data")
-
-// 	// check that a second new item is stored
-// 	queueTmp["url"] = "https://test-two.com"
-// 	expectedEndpt.URL = "https://test-two.com"
-// 	queueMsg, err = convertInterfaceToBytes(queueTmp)
-// 	th.Assert(t, err == nil, err)
-// 	err = saveMsgInDB(queueMsg, &args)
-// 	th.Assert(t, err == nil, err)
-// 	th.Assert(t, len(store.(*mock.BasicMockStore).FhirEndpointData) == 2, "there should be two endpoints in the database")
-// 	th.Assert(t, expectedEndpt.Equal(store.(*mock.BasicMockStore).FhirEndpointData[1]), "the second endpoint data does not equal expected store data")
-// 	expectedEndpt = testFhirEndpoint
-// 	queueTmp["url"] = "http://example.com/DTSU2/metadata"
-
-// 	// check that an item with the same URL updates the endpoint in the database
-// 	queueTmp["tlsVersion"] = "TLS 1.3"
-// 	queueMsg, err = convertInterfaceToBytes(queueTmp)
-// 	th.Assert(t, err == nil, err)
-// 	err = saveMsgInDB(queueMsg, &args)
-// 	th.Assert(t, err == nil, err)
-// 	th.Assert(t, len(store.(*mock.BasicMockStore).FhirEndpointData) == 2, "did not store data as expected")
-// 	th.Assert(t, store.(*mock.BasicMockStore).FhirEndpointData[0].TLSVersion == "TLS 1.3", "The TLS Version was not updated")
-
-// 	// check that error adding to store throws error
-// 	queueTmp["url"] = "https://a-new-url.com"
-// 	queueMsg, err = convertInterfaceToBytes(queueTmp)
-// 	th.Assert(t, err == nil, err)
-// 	addFn := store.(*mock.BasicMockStore).AddFHIREndpointFn
-// 	store.(*mock.BasicMockStore).AddFHIREndpointFn = func(_ context.Context, _ *endpointmanager.FHIREndpoint) error {
-// 		return errors.New("add fhir endpoint test error")
-// 	}
-// 	err = saveMsgInDB(queueMsg, &args)
-// 	th.Assert(t, errors.Cause(err).Error() == "add fhir endpoint test error", "expected error adding product")
-// 	store.(*mock.BasicMockStore).AddFHIREndpointFn = addFn
-
-// }
