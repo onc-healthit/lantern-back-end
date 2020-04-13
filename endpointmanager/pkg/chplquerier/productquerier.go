5ea55ad84e8692849d7cd0f3a34cd6ea260cbcbf
Lizzie Charbonneau <lizzie@mitre.org>
2020-04-13 10:17:55 -0400

    add debug statements

--- endpointmanager/pkg/chplquerier/productquerier.go
+++ endpointmanager/pkg/chplquerier/productquerier.go
@@ -135,9 +135,9 @@ func convertProductJSONToObj(ctx context.Context, prodJSON []byte) (*chplCertifi
 		// ok
 	}
 
-	fmt.Println("### PRINTING JSON ###\n")
-	fmt.Println("%v\n", prodJSON)
-	fmt.Println("### DONE PRINTING JSON ###\n")
+	fmt.Printf("### PRINTING JSON ###\n")
+	fmt.Printf("%v\n", prodJSON)
+	fmt.Printf("### DONE PRINTING JSON ###\n")
 	err := json.Unmarshal(prodJSON, &prodList)
 	if err != nil {
 		return nil, errors.Wrap(err, "unmarshalling the JSON into a chplCertifiedProductList object failed.")
