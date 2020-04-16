e0820982dc59b56cd48461f2d3fe6b0595c6cb22
Lizzie Charbonneau <lizzie@mitre.org>
2020-04-16 10:54:22 -0400

    updated views to use new table

--- scripts/sql/dbsetup.sql
+++ scripts/sql/dbsetup.sql
@@ -91,13 +91,15 @@ FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
 CREATE or REPLACE VIEW org_mapping AS
-SELECT endpts.url, endpts.vendor, endpts.organization_name AS endpoint_name, orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME, orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE, links.confidence AS MATCH_SCORE
+SELECT endpts.url, endpts_info.vendor, endpts.organization_name AS endpoint_name, orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME, orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE, links.confidence AS MATCH_SCORE
 FROM endpoint_organization AS links
 LEFT JOIN fhir_endpoints AS endpts ON links.endpoint_id = endpts.id
-LEFT JOIN npi_organizations AS orgs ON links.organization_id = orgs.id;
+LEFT JOIN npi_organizations AS orgs ON links.organization_id = orgs.id
+LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.id = endpts_info.fhir_endpoint_id;
 
 CREATE or REPLACE VIEW endpoint_export AS
-SELECT endpts.url, endpts.vendor, endpts.organization_name AS endpoint_name, endpts.tls_version, endpts.mime_types, endpts.http_response, endpts.capability_statement->>'fhirVersion' AS FHIR_VERSION, endpts.capability_statement->>'publisher' AS PUBLISHER, endpts.capability_statement->'software'->'name' AS SOFTWARE_NAME, endpts.capability_statement->'software'->'version' AS SOFTWARE_VERSION, endpts.capability_statement->'software'->'releaseDate' AS SOFTWARE_RELEASEDATE, orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME, orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE, links.confidence AS MATCH_SCORE
+SELECT endpts.url, endpts_info.vendor, endpts.organization_name AS endpoint_name, endpts_info.tls_version, endpts_info.mime_types, endpts_info.http_response, endpts_info.capability_statement->>'fhirVersion' AS FHIR_VERSION, endpts_info.capability_statement->>'publisher' AS PUBLISHER, endpts_info.capability_statement->'software'->'name' AS SOFTWARE_NAME, endpts_info.capability_statement->'software'->'version' AS SOFTWARE_VERSION, endpts_info.capability_statement->'software'->'releaseDate' AS SOFTWARE_RELEASEDATE, orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME, orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE, links.confidence AS MATCH_SCORE
 FROM endpoint_organization AS links
 RIGHT JOIN fhir_endpoints AS endpts ON links.endpoint_id = endpts.id
-LEFT JOIN npi_organizations AS orgs ON links.organization_id = orgs.id;
+LEFT JOIN npi_organizations AS orgs ON links.organization_id = orgs.id
+LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.id = endpts_info.fhir_endpoint_id;
