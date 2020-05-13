7069fd819cd2a066c7da7cd2ea179f8e6012db26
Lizzie Charbonneau <lizzie@mitre.org>
2020-05-13 06:48:49 -0400

    added tests to the helper functions and added comments to the helper functions

--- endpointmanager/pkg/helpers/helpers.go
+++ endpointmanager/pkg/helpers/helpers.go
@@ -2,6 +2,7 @@ package helpers
 
 import "sort"
 
+// StringArrayContains checks if the string array contains the provided string.
 func StringArrayContains(l []string, s string) bool {
 	for _, s2 := range l {
 		if s == s2 {
@@ -11,19 +12,22 @@ func StringArrayContains(l []string, s string) bool {
 	return false
 }
 
-func IntArrayContains(l []int, s int) bool {
-	for _, s2 := range l {
-		if s == s2 {
+// IntArrayContains checks if the integer array contains the provided integer.
+func IntArrayContains(l []int, i int) bool {
+	for _, i2 := range l {
+		if i == i2 {
 			return true
 		}
 	}
 	return false
 }
 
+// StringArraysEqual checks if l1 and l2 have the same contents regardless of order.
 func StringArraysEqual(l1 []string, l2 []string) bool {
 	if len(l1) != len(l2) {
 		return false
 	}
+
 	// don't care about order
 	a := make([]string, len(l1))
 	b := make([]string, len(l2))
