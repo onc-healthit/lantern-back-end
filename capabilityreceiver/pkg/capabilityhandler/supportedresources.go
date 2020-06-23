4342bf894295128fde34efff064f8aa890abf862
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2020-06-23 12:50:43 -0400

    Updated testing for supported resources

--- capabilityreceiver/pkg/capabilityhandler/supportedresources.go
+++ capabilityreceiver/pkg/capabilityhandler/supportedresources.go
@@ -5,6 +5,25 @@ func RunSupportedResourcesChecks(capInt map[string]interface{}) []string {
 		return nil
 	}
 	var supportedResources []string
-	supportedResources = append(supportedResources, "testing")
+
+	if capInt["rest"] == nil {
+		return nil
+	}
+	restArr := capInt["rest"].([]interface{})
+	restInt := restArr[0].(map[string]interface{})
+	if restInt["resource"] == nil {
+		return nil
+	}
+	resourceArr := restInt["resource"].([]interface{})
+
+	for _, resource := range resourceArr {
+		resourceInt := resource.(map[string]interface{})
+		if resourceInt["type"] == nil {
+			return nil
+		}
+		resourceType := resourceInt["type"].(string)
+		supportedResources = append(supportedResources, resourceType)
+	}
+
 	return supportedResources
 }
