b5ff5aa40cc4dd4bd10ab31815683e8d8afc6621
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2020-07-14 17:11:52 -0400

    Add performance tab back to shiny dashboard

---
+++ shinydashboard/lantern/functions/performance.R
@@ -0,0 +1,23 @@
+library(lubridate)
+
+get_avg_response_time <- function() {
+  # get time series of response time metrics for all endpoints
+  # will update with dynamic time ranges, group by 4 minute intervals
+  all_endpoints_response_time <- as_tibble(
+    tbl(db_connection,
+        sql("SELECT floor(extract(epoch from fhir_endpoints_info_history.entered_at)/240)*240 AS time, AVG(fhir_endpoints_info_history.response_time_seconds)
+            FROM fhir_endpoints_info, fhir_endpoints_info_history
+            WHERE fhir_endpoints_info.id = fhir_endpoints_info_history.id
+            AND fhir_endpoints_info_history.entered_at BETWEEN '2020-01-01T00:00:00Z' AND '2020-08-01T00:00:00Z'
+            GROUP BY time
+            ORDER BY time")
+        )
+    ) %>%
+    mutate(date = as_datetime(time)) %>%
+    select(date, avg)
+
+  # convert to xts format for use in dygraph
+  xts(x = all_endpoints_response_time$avg,
+      order.by = all_endpoints_response_time$date
+  )
+}
