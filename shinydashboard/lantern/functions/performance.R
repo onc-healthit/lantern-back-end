7594dbcf5b450955197b37ee641a0ade5da0d410
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2020-07-15 10:39:43 -0400

    Change query to only show data in range of 30 days

--- shinydashboard/lantern/functions/performance.R
+++ shinydashboard/lantern/functions/performance.R
@@ -2,14 +2,13 @@ library(lubridate)
 
 get_avg_response_time <- function() {
   # get time series of response time metrics for all endpoints
-  # groups by 23 hour intervals
+  # groups response time averages by 23 hour intervals and shows data for a range of 30 days
   all_endpoints_response_time <- as_tibble(
     tbl(db_connection,
-        sql("SELECT floor(extract(epoch from fhir_endpoints_info_history.entered_at)/82800)*82800 AS time, AVG(fhir_endpoints_info_history.response_time_seconds)
-            FROM fhir_endpoints_info_history
-            WHERE fhir_endpoints_info_history.entered_at BETWEEN '2020-01-01T00:00:00Z' AND '2020-08-01T00:00:00Z'
-            GROUP BY time
-            ORDER BY time")
+        sql("SELECT date.datetime AS time, AVG(fhir_endpoints_info_history.response_time_seconds)
+                FROM fhir_endpoints_info_history, (SELECT floor(extract(epoch from fhir_endpoints_info_history.entered_at)/82800)*82800 AS datetime FROM fhir_endpoints_info_history) as date
+                GROUP BY time HAVING date.datetime between(date.datetime+0) AND (date.datetime+2592000)
+                ORDER BY time")
         )
     ) %>%
     mutate(date = as_datetime(time)) %>%
