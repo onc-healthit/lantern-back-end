638c0fa0d149bd541e944b1e0f41465d493c4918
Brianna Mathiowetz <bmathiowetz@mitre.org>
2020-07-16 15:28:23 -0400

    Update fields page with caching changes.

--- shinydashboard/lantern/modules/fieldsmodule.r
+++ shinydashboard/lantern/modules/fieldsmodule.r
@@ -1,5 +1,4 @@
 # Fields Module
-library(treemapify)
 
 fieldsmodule_UI <- function(id) {
   
@@ -26,7 +25,7 @@ fieldsmodule_UI <- function(id) {
              tableOutput(ns("capstat_fields_table_optional"))),
       column(width=7,
              h4("Capability Statement Fields Count"),
-             plotOutput(ns("fields_bar_plot"))
+             uiOutput(ns("fields_plot"))
       )
     )
   )
@@ -44,7 +43,7 @@ fieldsmodule <- function(
   capstat_fields <- get_capstat_fields(db_connection)
 
   selected_fhir_endpoints <- reactive({
-    res <- capstat_fields
+    res <- app_data$capstat_fields
     req(sel_fhir_version(), sel_vendor())
     # If the selected dropdown value for the fhir verison is not the default "All FHIR Versions", filter
     # the capability statement fields by which fhir verison they're associated with
@@ -58,33 +57,59 @@ fieldsmodule <- function(
     res
   })
   
-  capstat_field_count <- reactive({get_capstat_fields_count(selected_fhir_endpoints())})
+  capstat_field_count <- reactive({
+    get_capstat_fields_count(selected_fhir_endpoints())
+  })
 
   # Required Capability Statement fields that we are tracking
   required_fields <- c("status", "kind", "fhirVersion", "format", "patchFormat")
 
   # Table of the required fields
-  output$capstat_fields_table_required <- renderTable(capstat_field_count() %>%
+  output$capstat_fields_table_required <- renderTable(
+    capstat_field_count() %>%
     relocate(exist) %>%
     filter(Fields %in% required_fields) %>%
-    rename("FHIR Version"=fhir_version))
+    rename("FHIR Version" = fhir_version)
+  )
 
   # Table of the optional fields
-  output$capstat_fields_table_optional <- renderTable(capstat_field_count() %>%
+  output$capstat_fields_table_optional <- renderTable(
+    capstat_field_count() %>%
     relocate(exist) %>%
     filter(!(Fields %in% required_fields)) %>%
-    rename("FHIR Version"=fhir_version))
-  
-  output$fields_bar_plot <- renderPlot({
-    df <- capstat_field_count()
-    ggplot(df,aes(x = fct_rev(as.factor(Fields)), y = Endpoints, fill = fhir_version)) +
-      geom_col(width = 0.8) +
-      theme(legend.position="top") +
-      theme(text = element_text(size = 14)) +
-      labs(x="",fill="FHIR Version") +
-      coord_flip()
-  },height = function() {
-    max(nrow(capstat_field_count()) * 24,100)
+    rename("FHIR Version"=fhir_version)
+  )
+
+  vendor <- reactive({
+    sel_vendor()
   })
+
+  plot_height <- reactive({
+    max(nrow(capstat_field_count()) * 25, 400)
+  })
+
+  output$fields_plot <- renderUI({
+    tagList(
+      plotOutput(ns("fields_bar_plot"), height = plot_height())
+    )
+  })
+  
+  output$fields_bar_plot <- renderCachedPlot({
+    ggplot(capstat_field_count(), aes(x = fct_rev(as.factor(Fields)), y = Endpoints, fill = fhir_version)) +
+      geom_col(width = 0.8) +
+      theme(legend.position = "top") +
+      theme(text = element_text(size = 14)) +
+      labs(x="", y = "Number of Endpoints", fill = "FHIR Version", title = vendor()) +
+      coord_flip()
+  },
+    sizePolicy = sizeGrowthRatio(width = 400,
+                                  height = 400,
+                                  growthRate = 1.2),
+    res = 72,
+    cache = "app",
+    cacheKeyExpr = {
+      list(sel_fhir_version(), sel_vendor(), app_data$last_updated)
+    }
+  )
   
 }
