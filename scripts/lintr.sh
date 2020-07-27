e09f2d0f139a9207d7bd4f8be7a7e3ec3ab5e74d
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2020-07-27 17:07:55 -0400

    change bin/sh to bin/bash

--- scripts/lintr.sh
+++ scripts/lintr.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 cd ../shinydashboard/lantern
 echo "Running R lintr"
 LINTR=$(Rscript -e "lintr::lint_dir(linters = lintr::with_defaults(object_usage_linter=NULL, closed_curly_linter = NULL, open_curly_linter = NULL, line_length_linter = NULL, object_name_linter = NULL))")
