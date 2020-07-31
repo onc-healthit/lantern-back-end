22544a1c5f2946a629d9563727601af7a85bdcf6
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2020-07-31 08:57:14 -0400

    Prints message to install R and R lintr package if lintr command fails due to lintr not being installed

--- scripts/lintr.sh
+++ scripts/lintr.sh
@@ -2,7 +2,7 @@
 
 cd ../shinydashboard/lantern
 echo "Running R lintr"
-LINTR=$(Rscript -e "lintr::lint_dir(linters = lintr::with_defaults(object_usage_linter=NULL, closed_curly_linter = NULL, open_curly_linter = NULL, line_length_linter = NULL, object_name_linter = NULL))")
+LINTR=$(Rscript -e "lintr::lint_dir(linters = lintr::with_defaults(object_usage_linter=NULL, closed_curly_linter = NULL, open_curly_linter = NULL, line_length_linter = NULL, object_name_linter = NULL))") || echo "Make sure you have R and the R lintr package installed, instructions in README"
 if [[ ! -z "${LINTR[0]}" ]]; then
     for i in "${LINTR[@]}"
     do
