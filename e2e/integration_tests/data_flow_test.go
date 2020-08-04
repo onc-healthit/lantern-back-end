71255e30075a3ad9cfc5a1c9173a509d592d1960
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2020-08-04 15:00:21 -0400

    Fix data_flow test typo

--- e2e/integration_tests/data_flow_test.go
+++ e2e/integration_tests/data_flow_test.go
@@ -215,7 +215,7 @@ func Test_EndpointLinksAreAvailable(t *testing.T) {
 	}
 
 	ctx := context.Background()
-	endpointlinker.LinkAllOrgsAndEndpoints(ctx, store, "", "")
+	endpointlinker.LinkAllOrgsAndEndpoints(ctx, store, "", "", false)
 
 	endpoint_orgs_row = store.DB.QueryRow("SELECT COUNT(*) FROM endpoint_organization;")
 	err = endpoint_orgs_row.Scan(&link_count)
@@ -532,7 +532,7 @@ func Test_RetrieveCapabilityStatements(t *testing.T) {
 		t.Fatalf("Only %d endpoints should be in fhir_endpoints after updating with file %s, Got: %d", expected_endpt_ct, shortEndptList, endpt_count)
 	}
 
-	endpointlinker.LinkAllOrgsAndEndpoints(ctx, store, "", "")
+	endpointlinker.LinkAllOrgsAndEndpoints(ctx, store, "", "", false)
 
 	// Check that links were not deleted on update in order to maintain previous mappings from endpoints
 	// to organizations
