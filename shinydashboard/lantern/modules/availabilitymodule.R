9576a5746bbb968b39eddea0eeee984f3ec183f0
Brianna Mathiowetz <bmathiowetz@mitre.org>
2020-08-05 15:55:14 -0400

    Update legend title, move legend, and add url to bar chart tooltip.

--- shinydashboard/lantern/modules/availabilitymodule.R
+++ shinydashboard/lantern/modules/availabilitymodule.R
@@ -22,9 +22,13 @@ availability <- function(
   # to be factors so they can be shown as separate categories on the
   # graph, rather than as a scalar value
 
+  plot <- ggplotly(ggplot(app_data$http_pctf, aes(x = name, y = Percentage, fill = Code, label = url)) +
+               geom_bar(stat = "identity") + ggtitle("Endpoints returning non-HTTP 200 responses") +
+               theme(legend.title = element_blank()))
+  formatted_plot <- plot %>% layout(legend = list(title=list(text='HTTP <br>Status <br>Code'), x = 1, y = 0.5))
+
   output$non_200 <- renderPlotly({
-    ggplotly(ggplot(app_data$http_pctf, aes(x = name, y = Percentage, fill = code_f)) +
-               geom_bar(stat = "identity") + ggtitle("Endpoints returning non-HTTP 200 responses"))
+    formatted_plot
   })
 
   output$count_200_sub <- renderText({
