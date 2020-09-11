df159ad6ccce72b9d5c73a0ed18e130d5a1ca1fa
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2020-09-11 09:05:01 -0400

    Fix lintR error

--- shinydashboard/lantern/server.R
+++ shinydashboard/lantern/server.R
@@ -1,6 +1,6 @@
 # Define server function
 function(input, output, session) {
-  
+
   # Trigger this observer every time the session changes, which is on first load of page, and switch tab to tab stored in url
   observeEvent(session, {
     query <- parseQueryString(session$clientData$url_search)
