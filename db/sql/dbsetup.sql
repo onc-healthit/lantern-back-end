572b49e5d7d782d46756ef702fb9846d03d0a5d3
Matt Mayer <mmayer@mitre.org>
2020-09-18 16:56:01 -0400

    Add indexes to dbsetup

--- db/sql/dbsetup.sql
+++ db/sql/dbsetup.sql
@@ -296,3 +296,15 @@ RIGHT JOIN fhir_endpoints AS endpts ON links.url = endpts.url
 LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
 LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id
 LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id;
+
+CREATE INDEX fhir_endpoints_url_idx ON fhir_endpoints (url);
+CREATE INDEX fhir_endpoints_info_url_idx ON fhir_endpoints_info (url);
+CREATE INDEX fhir_endpoints_info_history_url_idx ON fhir_endpoints_info_history (url);
+CREATE INDEX endpoint_organization_url_idx ON endpoint_organization (url);
+
+CREATE INDEX vendor_id_idx ON vendors (id);
+CREATE INDEX fhir_endpoints_info_vendor_id_idx ON fhir_endpoints_info (vendor_id);
+CREATE INDEX fhir_endpoints_info_history_vendor_id_idx ON fhir_endpoints_info_history (vendor_id);
+
+CREATE INDEX npi_organizations_npi_id_idx ON npi_organizations (npi_id);
+CREATE INDEX endpoint_organization_npi_id_idx ON endpoint_organization (organization_npi_id);
