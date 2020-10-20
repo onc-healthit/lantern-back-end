d6a9f7e181b9a9f9fed339a34e5ea46fa22c0e08
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2020-10-20 11:01:07 -0400

    Move new index migrations to new migration script

--- db/migration/migrations/000004_add_indexes.up.sql
+++ db/migration/migrations/000004_add_indexes.up.sql
@@ -12,36 +12,4 @@ CREATE INDEX fhir_endpoints_info_history_vendor_id_idx ON fhir_endpoints_info_hi
 CREATE INDEX npi_organizations_npi_id_idx ON npi_organizations (npi_id);
 CREATE INDEX endpoint_organization_npi_id_idx ON endpoint_organization (organization_npi_id);
 
-CREATE INDEX fhir_version_idx ON fhir_endpoints_info ((capability_statement->>'fhirVersion'));
-CREATE INDEX implementation_guide_idx ON fhir_endpoints_info ((capability_statement->>'implementationGuide'));
-CREATE INDEX field_idx ON fhir_endpoints_info ((included_fields->> 'Field'));
-CREATE INDEX exists_idx ON fhir_endpoints_info ((included_fields->> 'Exists'));
-CREATE INDEX extension_idx ON fhir_endpoints_info ((included_fields->> 'Extension'));
-
-CREATE INDEX resource_type_idx ON fhir_endpoints_info (((capability_statement::json#>'{rest,0,resource}') ->> 'type'));
-
-CREATE INDEX capstat_url_idx ON fhir_endpoints_info ((capability_statement->>'url'));
-CREATE INDEX capstat_version_idx ON fhir_endpoints_info ((capability_statement->>'version'));
-CREATE INDEX capstat_name_idx ON fhir_endpoints_info ((capability_statement->>'name'));
-CREATE INDEX capstat_title_idx ON fhir_endpoints_info ((capability_statement->>'title'));
-CREATE INDEX capstat_date_idx ON fhir_endpoints_info ((capability_statement->>'date'));
-CREATE INDEX capstat_publisher_idx ON fhir_endpoints_info ((capability_statement->>'publisher'));
-CREATE INDEX capstat_description_idx ON fhir_endpoints_info ((capability_statement->>'description'));
-CREATE INDEX capstat_purpose_idx ON fhir_endpoints_info ((capability_statement->>'purpose'));
-CREATE INDEX capstat_copyright_idx ON fhir_endpoints_info ((capability_statement->>'copyright'));
-
-CREATE INDEX capstat_software_name_idx ON fhir_endpoints_info ((capability_statement->'software'->>'name'));
-CREATE INDEX capstat_software_version_idx ON fhir_endpoints_info ((capability_statement->'software'->>'version'));
-CREATE INDEX capstat_software_releaseDate_idx ON fhir_endpoints_info ((capability_statement->'software'->>'releaseDate'));
-CREATE INDEX capstat_implementation_description_idx ON fhir_endpoints_info ((capability_statement->'implementation'->>'description'));
-CREATE INDEX capstat_implementation_url_idx ON fhir_endpoints_info ((capability_statement->'implementation'->>'url'));
-CREATE INDEX capstat_implementation_custodian_idx ON fhir_endpoints_info ((capability_statement->'implementation'->>'custodian'));
-
-CREATE INDEX security_code_idx ON fhir_endpoints_info ((capability_statement::json#>'{rest,0,security,service}'->'coding'->>'code'));
-CREATE INDEX security_service_idx ON fhir_endpoints_info ((capability_statement::json#>'{rest,0,security}' -> 'service' ->> 'text'));
-
-CREATE INDEX smart_capabilities_idx ON fhir_endpoints_info ((smart_response->'capabilities'));
-
-CREATE INDEX location_zipcode_idx ON npi_organizations ((location->>'zipcode'));
-
 COMMIT;
