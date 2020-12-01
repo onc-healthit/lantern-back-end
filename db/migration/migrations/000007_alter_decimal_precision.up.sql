dd8e3e9858dcdf39857e77bc3c3dd9df8b62477a
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2020-12-01 10:28:06 -0500

    Fix migration repeated migration script number; create new migration script to alter decimal precision

---
+++ db/migration/migrations/000007_alter_decimal_precision.up.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+DROP VIEW IF EXISTS endpoint_export;
+
+ALTER TABLE fhir_endpoints_info ALTER COLUMN availability TYPE DECIMAL(5,4);
+ALTER TABLE fhir_endpoints_info ALTER COLUMN response_time_seconds TYPE DECIMAL(7,4);
+
+ALTER TABLE fhir_endpoints_info_history ALTER COLUMN availability TYPE DECIMAL(5,4);
+ALTER TABLE fhir_endpoints_info_history ALTER COLUMN response_time_seconds TYPE DECIMAL(7,4);
+
+CREATE or REPLACE VIEW endpoint_export AS
+SELECT endpts.url, endpts.list_source, endpts.organization_names AS endpoint_names,
+    vendors.name as vendor_name,
+    endpts_info.tls_version, endpts_info.mime_types, endpts_info.http_response,
+    endpts_info.response_time_seconds, endpts_info.smart_http_response, endpts_info.errors,
+    endpts_info.capability_statement->>'fhirVersion' AS FHIR_VERSION,
+    endpts_info.capability_statement->>'publisher' AS PUBLISHER,
+    endpts_info.capability_statement->'software'->'name' AS SOFTWARE_NAME,
+    endpts_info.capability_statement->'software'->'version' AS SOFTWARE_VERSION,
+    endpts_info.capability_statement->'software'->'releaseDate' AS SOFTWARE_RELEASEDATE,
+    endpts_info.updated_at AS INFO_UPDATED, endpts_info.created_at AS INFO_CREATED,
+    orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME,
+    orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE,
+    links.confidence AS MATCH_SCORE, endpts_info.supported_resources
+FROM endpoint_organization AS links
+RIGHT JOIN fhir_endpoints AS endpts ON links.url = endpts.url
+LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
+LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id
+LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id;
+
+COMMIT;
