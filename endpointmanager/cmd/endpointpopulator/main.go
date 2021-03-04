78a48b8b5bdb7b951664981e992698bdfd829f3d
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2021-03-04 15:46:02 -0500

    Fix wording of warning message; add endpoint total check after nppescontactpopulator is run

--- endpointmanager/cmd/endpointpopulator/main.go
+++ endpointmanager/cmd/endpointpopulator/main.go
@@ -58,7 +58,7 @@ func main() {
 		helpers.FailOnError("", err)
 
 		if endpointTotal >= maxEndpoints {
-			log.Warn("The current number of endpoints exceeds the amount that can be queried in the given Lantern query interval. Make sure to either scale out query interval as defined in the README, or define a longer query threshold.")
+			log.Warn("The current number of endpoints exceeds the maximum amount of endpoints that can be queried within the given Lantern query interval. Make sure to either scale out the capability querier service as defined in the README, or define a longer query threshold.")
 		}
 	}
 }
--- endpointmanager/cmd/nppescontactpopulator/main.go
+++ endpointmanager/cmd/nppescontactpopulator/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"math"
 	"os"
 
 	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/helpers"
@@ -33,4 +34,16 @@ func main() {
 	added, err := nppesquerier.ParseAndStoreNPIContactsFile(ctx, fname, store)
 	log.Infof("Added %d NPI FHIR URLs to database\n", added)
 	helpers.FailOnError("", err)
+
+	queryInterval := viper.GetInt("capquery_qryintvl")
+	maxEndpoints := int(math.Round(float64(queryInterval*60) / float64(1.5)))
+
+	var endpointTotal int
+	endpointCountQuery := "SELECT COUNT(*) from fhir_endpoints;"
+	err = store.DB.QueryRow(endpointCountQuery).Scan(&endpointTotal)
+	helpers.FailOnError("", err)
+
+	if endpointTotal >= maxEndpoints {
+		log.Warn("The current number of endpoints exceeds the maximum amount of endpoints that can be queried within the given Lantern query interval. Make sure to either scale out the capability querier service as defined in the README, or define a longer query threshold.")
+	}
 }
