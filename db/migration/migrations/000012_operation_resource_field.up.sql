fc898d20018f2d559a4585bedade8d716b6360b9
Brianna Mathiowetz <bmathiowetz@mitre.org>
2021-03-31 13:32:32 -0400

    add/remove supported_resources and update endpoint_export function in migration scripts

--- db/migration/migrations/000012_operation_resource_field.up.sql
+++ db/migration/migrations/000012_operation_resource_field.up.sql
@@ -1,6 +1,32 @@
 BEGIN;
 
+DROP VIEW IF EXISTS endpoint_export;
+
 ALTER TABLE fhir_endpoints_info ADD COLUMN operation_resource JSONB;
 ALTER TABLE fhir_endpoints_info_history ADD COLUMN operation_resource JSONB;
+
+ALTER TABLE fhir_endpoints_info DROP COLUMN IF EXISTS supported_resources CASCADE;
+ALTER TABLE fhir_endpoints_info_history DROP COLUMN IF EXISTS supported_resources CASCADE;
+
+CREATE or REPLACE VIEW endpoint_export AS
+SELECT endpts.url, endpts.list_source, endpts.organization_names AS endpoint_names,
+    vendors.name as vendor_name,
+    endpts_info.tls_version, endpts_info.mime_types, endpts_metadata.http_response,
+    endpts_metadata.response_time_seconds, endpts_metadata.smart_http_response, endpts_metadata.errors,
+    endpts_info.capability_statement->>'fhirVersion' AS FHIR_VERSION,
+    endpts_info.capability_statement->>'publisher' AS PUBLISHER,
+    endpts_info.capability_statement->'software'->'name' AS SOFTWARE_NAME,
+    endpts_info.capability_statement->'software'->'version' AS SOFTWARE_VERSION,
+    endpts_info.capability_statement->'software'->'releaseDate' AS SOFTWARE_RELEASEDATE,
+    endpts_info.updated_at AS INFO_UPDATED, endpts_info.created_at AS INFO_CREATED,
+    orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME,
+    orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE,
+    links.confidence AS MATCH_SCORE, endpts_metadata.availability
+FROM endpoint_organization AS links
+RIGHT JOIN fhir_endpoints AS endpts ON links.url = endpts.url
+LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
+LEFT JOIN fhir_endpoints_metadata AS endpts_metadata ON endpts_info.metadata_id = endpts_metadata.id
+LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id
+LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id;
 
 COMMIT;
