cf0d1a0edbfbe6987afc83fd402016ba15ea2a2e
Matt Mayer <mmayer@mitre.org>
2021-04-01 11:24:37 -0400

    Remove versionsresponse

--- endpointmanager/pkg/versionsoperation/versionsresponse.go
+++
@@ -1,8 +0,0 @@
-package versionsoperation
-
-// base struct to handle any methods that don't change between the versions of FHIR
-// capability statements
-type VersionsResponse struct {
-	versions []string		`json:"versions"`
-	defaultVersion string	`json:"default"`
-}
