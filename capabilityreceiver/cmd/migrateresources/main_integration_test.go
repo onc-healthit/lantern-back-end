c1900171bda31373b73b79a9f5196b78e65b138a
Brianna Mathiowetz <bmathiowetz@mitre.org>
2021-05-10 15:08:55 -0400

    Realized migration resources integration test was failing silently

--- capabilityreceiver/cmd/migrateresources/main_integration_test.go
+++ capabilityreceiver/cmd/migrateresources/main_integration_test.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -20,8 +21,8 @@ import (
 
 var store *postgresql.Store
 
-var capStat1 capabilityparser.CapabilityStatement
-var capStat2 capabilityparser.CapabilityStatement
+var capStat1 []byte
+var capStat2 []byte
 
 func TestMain(m *testing.M) {
 	var err error
@@ -55,38 +56,40 @@ func Test_updateOperationResource(t *testing.T) {
 	setupCapabilityStatements(t, filepath.Join("../../testdata", "cerner_capability_dstu2.json"), filepath.Join("../../testdata", "epic_capability_dstu2.json"))
 	ctx := context.Background()
 
-	addFHIREndpointInfoHistoryStatement, err := store.DB.Prepare(`
+	addFHIREndpointInfoHistoryStatement := `
 		INSERT INTO fhir_endpoints_info_history (
 			url,
 			capability_statement,
-			entered_at
+			operation,
+			updated_at
 		)
-		VALUES ($1, $2, $3)`)
-	th.Assert(t, err == nil, "addFHIREndpointInfoHistoryStatement should not have returned an error")
-	defer addFHIREndpointInfoHistoryStatement.Close()
+		VALUES ($1, $2, $3, $4)`
 
 	getFHIREndpointInfoHistoryStatement := `
-		SELECT entered_at, operation_resource
+		SELECT updated_at, operation_resource
 		FROM fhir_endpoints_info_history
 		WHERE url=$1
 	`
 	// Put two FHIR endpoints in the history table
-	firstTime := time.Now()
+	firstTime := time.Now().UTC()
 	url1 := "www.testurl.com/cerner/DSTU2"
-	addFHIREndpointInfoHistoryStatement.ExecContext(ctx, url1, capStat1, firstTime)
+	_, err := store.DB.ExecContext(ctx, addFHIREndpointInfoHistoryStatement, url1, capStat1, "I", firstTime)
+	th.Assert(t, err == nil, fmt.Sprintf("Error when adding to the database %s", err))
 
-	secondTime := time.Now()
+	secondTime := time.Now().UTC()
 	url2 := "www.testurl.com/epic/DSTU2"
-	addFHIREndpointInfoHistoryStatement.ExecContext(ctx, url2, capStat2, secondTime)
+	_, err = store.DB.ExecContext(ctx, addFHIREndpointInfoHistoryStatement, url2, capStat2, "I", secondTime)
+	th.Assert(t, err == nil, fmt.Sprintf("Error when adding to the database again %s", err))
 
 	resultCh := make(chan Result)
 
 	// Check that data only updates the first URL
 	defaultArgs := make(map[string]interface{})
 	defaultArgs["historyArgs"] = historyArgs{
-		fhirURL: url1,
-		store:   store,
-		result:  resultCh,
+		fhirURL:   url1,
+		store:     store,
+		result:    resultCh,
+		isHistory: true,
 	}
 
 	go updateOperationResource(ctx, &defaultArgs)
@@ -101,12 +104,16 @@ func Test_updateOperationResource(t *testing.T) {
 	defer historyRows.Close()
 	count := 0
 	for historyRows.Next() {
-		th.Assert(t, count <= 1, fmt.Sprintf("should only be one item in the database for this URL"))
+		th.Assert(t, count < 1, fmt.Sprintf("should only be one item in the database for this URL"))
 		var receivedTime time.Time
-		var operationResource map[string][]string
-		err = historyRows.Scan(&receivedTime, &operationResource)
+		var operationResJSON []byte
+		err = historyRows.Scan(&receivedTime, &operationResJSON)
 		th.Assert(t, err == nil, fmt.Sprintf("Error while scanning the rows of the history table for URL %s. Error: %s", url1, err))
-		th.Assert(t, receivedTime == firstTime, fmt.Sprintf("The time was updated to %+v", receivedTime))
+		th.Assert(t, receivedTime.Equal(firstTime), fmt.Sprintf("The time was updated to %+v from %+v", receivedTime, firstTime))
+		th.Assert(t, operationResJSON != nil, "Operation resource value should not be nil")
+		var operationResource map[string][]string
+		err = json.Unmarshal(operationResJSON, &operationResource)
+		th.Assert(t, err == nil, fmt.Sprintf("Error unmarshalling: %s", err))
 		th.Assert(t, len(operationResource) == 2, fmt.Sprintf("The number of operation resources should have been 2. Is instead %d", len(operationResource)))
 		th.Assert(t, operationResource["read"] != nil, "There should be a read resource defined, instead is nil")
 		th.Assert(t, operationResource["search-type"] != nil, "There should be a search-type resource defined, instead is nil")
@@ -114,10 +121,12 @@ func Test_updateOperationResource(t *testing.T) {
 		th.Assert(t, len(operationResource["search-type"]) == 23, fmt.Sprintf("The number of operation resources should have been 23. Is instead %d", len(operationResource["search-type"])))
 		count++
 	}
+	th.Assert(t, count == 1, "should be one item in the database, instead is 0")
 
 	// Add another instance of the second URL
-	thirdTime := time.Now()
-	addFHIREndpointInfoHistoryStatement.ExecContext(ctx, url2, capStat2, thirdTime)
+	thirdTime := time.Now().UTC()
+	_, err = store.DB.ExecContext(ctx, addFHIREndpointInfoHistoryStatement, url2, capStat2, "U", thirdTime)
+	th.Assert(t, err == nil, fmt.Sprintf("Error when adding to the database third time %s", err))
 
 	// Make sure all instances of that are updated
 	resultCh2 := make(chan Result)
@@ -125,9 +134,10 @@ func Test_updateOperationResource(t *testing.T) {
 	// Check that data only updates the first URL
 	defaultArgs2 := make(map[string]interface{})
 	defaultArgs2["historyArgs"] = historyArgs{
-		fhirURL: url2,
-		store:   store,
-		result:  resultCh2,
+		fhirURL:   url2,
+		store:     store,
+		result:    resultCh2,
+		isHistory: true,
 	}
 
 	go updateOperationResource(ctx, &defaultArgs2)
@@ -143,9 +153,13 @@ func Test_updateOperationResource(t *testing.T) {
 	for historyRows.Next() {
 		th.Assert(t, count <= 2, fmt.Sprintf("should be two items in the database for this URL"))
 		var receivedTime time.Time
-		var operationResource map[string][]string
-		err = historyRows.Scan(&receivedTime, &operationResource)
+		var operationResJSON []byte
+		err = historyRows.Scan(&receivedTime, &operationResJSON)
 		th.Assert(t, err == nil, fmt.Sprintf("Error while scanning the rows of the history table for URL %s. Error: %s", url1, err))
+		th.Assert(t, operationResJSON != nil, "Operation resource value should not be nil")
+		var operationResource map[string][]string
+		err = json.Unmarshal(operationResJSON, &operationResource)
+		th.Assert(t, err == nil, fmt.Sprintf("Error unmarshalling: %s", err))
 		th.Assert(t, len(operationResource) == 2, fmt.Sprintf("The number of operation resources should have been 2. Is instead %d", len(operationResource)))
 		th.Assert(t, operationResource["read"] != nil, "There should be a read resource defined, instead is nil")
 		th.Assert(t, operationResource["search-type"] != nil, "There should be a search-type resource defined, instead is nil")
@@ -153,6 +167,7 @@ func Test_updateOperationResource(t *testing.T) {
 		th.Assert(t, len(operationResource["search-type"]) == 16, fmt.Sprintf("The number of operation resources should have been 16. Is instead %d", len(operationResource["search-type"])))
 		count++
 	}
+	th.Assert(t, count == 2, "should be two items in the database, instead is 1")
 }
 
 func setupCapabilityStatements(t *testing.T, path1 string, path2 string) {
@@ -161,13 +176,15 @@ func setupCapabilityStatements(t *testing.T, path1 string, path2 string) {
 	th.Assert(t, err == nil, err)
 	cs, err := capabilityparser.NewCapabilityStatement(csJSON)
 	th.Assert(t, err == nil, err)
-	capStat1 = cs
+	capStat1, err = cs.GetJSON()
+	th.Assert(t, err == nil, err)
 
 	csJSON2, err := ioutil.ReadFile(path2)
 	th.Assert(t, err == nil, err)
 	cs2, err := capabilityparser.NewCapabilityStatement(csJSON2)
 	th.Assert(t, err == nil, err)
-	capStat2 = cs2
+	capStat2, err = cs2.GetJSON()
+	th.Assert(t, err == nil, err)
 }
 
 func setup() error {
