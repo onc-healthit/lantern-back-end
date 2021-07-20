50b8d06a9afb7ced79a0006b040b8760181c0372
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2021-07-20 16:38:28 -0400

    Add validation result count plot and validation failure table to validation tab

--- shinydashboard/lantern/modules/validationsmodule.R
+++ shinydashboard/lantern/modules/validationsmodule.R
@@ -7,14 +7,26 @@ validationsmodule_UI <- function(id) {
 
   tagList(
     fluidRow(
-      column(width = 12, style = "padding-bottom:20px",
-             h3(style = "margin-top:0", textOutput(ns("endpoint_count"))),
-             downloadButton(ns("download_data"), "Download Endpoint Data (CSV)"),
-             downloadButton(ns("download_descriptions"), "Download Field Descriptions (CSV)")
-      ),
-    ),
-    DT::dataTableOutput(ns("endpoints_table")),
-    htmlOutput(ns("note_text"))
+      column(width = 4,
+            "sidebar",
+             h3("Validation Details"),
+             DT::dataTableOutput(ns("validation_details_table"))
+            ),
+      column(width = 8,
+          "main",
+          fluidRow(
+            column(width = 12,
+                h3("Validation Results Count"),
+                uiOutput(ns("validation_results_plot"))
+            )
+          ), fluidRow(
+            column(width = 12,
+              h3("Validation Failure Details"),
+              DT::dataTableOutput(ns("validation_failure_table"))
+            )
+          )
+      )
+    )
   )
 }
 
@@ -23,8 +35,8 @@ validationsmodule <- function(
   output,
   session,
   sel_fhir_version,
-  sel_vendor,
-  sel_availability
+  sel_vendor, 
+  sel_validation_group
 ) {
   ns <- session$ns
 
@@ -32,79 +44,124 @@ validationsmodule <- function(
     paste("Matching Endpoints:", nrow(selected_fhir_endpoints()))
   })
 
-  selected_fhir_endpoints <- reactive({
-    res <- get_fhir_endpoints_tbl()
-    req(sel_fhir_version(), sel_vendor())
+  validation_rules <- reactive ({
+    res <- isolate(app_data$validation_tbl())
+    res <- res %>%
+           filter(reference != "") %>%
+           mutate(rule_name = paste("Name:", rule_name)) %>%
+           mutate(comment = paste("Comment:", comment)) %>%
+           mutate(num = row_number()) %>%
+           distinct(num, rule_name, comment)
+    x <- data.frame("validation_details" = c(rbind(res$num, res$rule_name, res$comment)))
+    x
+  })
+
+  selected_validations <- reactive({
+    res <- isolate(app_data$validation_tbl())
+    req(sel_fhir_version(), sel_vendor(), sel_validation_group())
     if (sel_fhir_version() != ui_special_values$ALL_FHIR_VERSIONS) {
       res <- res %>% filter(fhir_version == sel_fhir_version())
+    }
+    if (sel_validation_group() != "All Groups") {
+      if (sel_validation_group() == "Other") {
+        res <- res %>% filter(reference == "")
+      } else if (sel_validation_group() == "HTTP") {
+        res <- res %>% filter(reference == "http://hl7.org/fhir/http.html")
+      } else if (sel_validation_group() == "Capability Statements") {
+        res <- res %>% filter(reference == "http://hl7.org/fhir/capabilitystatement.html")
+      } else if (sel_validation_group() == "SMART") {
+        res <- res %>% filter(reference == "http://www.hl7.org/fhir/smart-app-launch/conformance/index.html")
+      } else if (sel_validation_group() == "US-CORE") {
+        res <- res %>% filter(reference == "https://www.hl7.org/fhir/us/core/CapabilityStatement-us-core-server.html" | reference == "https://www.hl7.org/fhir/us/core/security.html")
+      } else if (sel_validation_group() == "Certification Criteria") {
+        res <- res %>% filter(reference == "https://www.healthit.gov/cures/sites/default/files/cures/2020-03/APICertificationCriterion.pdf")
+      }
     }
     if (sel_vendor() != ui_special_values$ALL_DEVELOPERS) {
       res <- res %>% filter(vendor_name == sel_vendor())
     }
-    if (sel_availability() != "0-100") {
-      if (sel_availability() == "0" || sel_availability() == "100") {
-        availability_filter_num <- as.numeric(sel_availability()) / 100
-        availability_filter <- as.character(availability_filter_num)
-        res <- res %>% filter(availability == availability_filter)
-      }
-      else {
-        availability_upper_num <- as.numeric(strsplit(sel_availability(), "-")[[1]][2]) / 100
-        availability_lower_num <- as.numeric(strsplit(sel_availability(), "-")[[1]][1]) / 100
-        availability_lower <- as.character(availability_lower_num)
-        availability_upper <- as.character(availability_upper_num)
-
-        res <- res %>% filter(availability >= availability_lower, availability <= availability_upper)
-      }
-    }
-    res <- res %>% mutate(availability = availability * 100)
     res
   })
 
-  # Downloadable csv of selected dataset
-  output$download_data <- downloadHandler(
-    filename = function() {
-      "fhir_endpoints.csv"
-    },
-    content = function(file) {
-      write.csv(csv_format(), file, row.names = FALSE)
-    }
-  )
+  select_validation_results <- reactive ({
+    res <- selected_validations()
+    res <- res %>% 
+            group_by(rule_name, valid) %>%
+            count() %>%
+            rename(count = n) %>%
+            select(rule_name, valid, count)
+    res
 
-  # Download csv of the field descriptions in the dataset csv
-  output$download_descriptions <- downloadHandler(
-    filename = function() {
-      "fhir_endpoints_fields.csv"
-    },
-    content = function(file) {
-      file.copy("fhir_endpoints_fields.csv", file)
-    }
-  )
+  })
 
-  output$endpoints_table <- DT::renderDataTable({
-    datatable(selected_fhir_endpoints() %>% select(url, endpoint_names, updated, vendor_name, fhir_version, tls_version, mime_types, status, availability),
-              colnames = c("URL", "API Information Source Name", "Updated", "Certified API Developer Name", "FHIR Version", "TLS Version", "MIME Types", "HTTP Response", "Availability"),
+  failed_validation_results <- reactive ({
+    res <- selected_validations()
+    res <- res %>% 
+          filter(valid == FALSE)
+    res 
+  })
+
+  output$validation_details_table <- DT::renderDataTable({
+    datatable(validation_rules() %>% select(validation_details),
+              colnames = c("Rules"),
+              rownames = FALSE,
+              options = list(scrollX = TRUE, scrollY = 700, paging = FALSE, dom = 't', ordering = FALSE)
+            )
+  })
+
+  validation_plot_height <- reactive({
+    max(nrow(select_validation_results()) * 25, 400)
+  })
+
+  output$validation_results_plot <- renderUI({
+    if (nrow(select_validation_results()) != 0) {
+      tagList(
+        plotOutput(ns("validation_bar_plot"), height = validation_plot_height())
+      )
+    } else {
+      tagList(
+        plotOutput(ns("validation_bar_empty_plot"), height = validation_plot_height())
+      )
+    }
+  })
+  output$validation_bar_plot <- renderCachedPlot({
+    ggplot(select_validation_results(), aes(x = fct_rev(as.factor(rule_name)), y = count, fill = valid)) +
+      geom_col(width = 0.8) +
+      geom_text(aes(label = stat(y)), position = position_stack(vjust = 0.5)) +
+      ggtitle("Validation Results") +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      theme(legend.position = "bottom") +
+      theme(text = element_text(size = 14)) +
+      labs(x = "", y = "", fill = "Valid") +
+      scale_y_continuous(sec.axis = sec_axis(~.)) +
+      scale_fill_manual(values = c("red", "seagreen3")) +
+      guides(fill = guide_legend(reverse = TRUE)) +
+      coord_flip()
+  },
+    sizePolicy = sizeGrowthRatio(width = 400,
+                                  height = 400,
+                                  growthRate = 1.2),
+    res = 72,
+    cache = "app",
+    cacheKeyExpr = {
+      list(sel_fhir_version(), sel_vendor(), sel_validation_group(), app_data$last_updated())
+    })
+
+  output$validation_bar_empty_plot <- renderPlot({
+    ggplot(select_validation_results()) +
+    geom_col(width = 0.8) +
+    labs(x = "", y = "") +
+    theme(axis.text.x = element_blank(),
+    axis.text.y = element_blank(), axis.ticks = element_blank()) +
+    annotate("text", label = "There are no validation results for the endpoints\nthat pass the selected filtering criteia", x = 1, y = 2, size = 4.5, colour = "red", hjust = 0.5)
+  })
+
+    output$validation_failure_table <- DT::renderDataTable({
+    datatable(failed_validation_results() %>% select(url, expected, actual, vendor_name, fhir_version),
+              colnames = c("URL", "Expected Value", "Actual Value", "Certified API Developer Name", "FHIR Version"),
               rownames = FALSE,
               options = list(scrollX = TRUE)
-    )
-  })
-
-  # Create the format for the csv
-  csv_format <- reactive({
-    res <- selected_fhir_endpoints() %>%
-      select(-updated, -label, -status, -availability) %>%
-      rename(api_information_source_name = endpoint_names, certified_api_developer_name = vendor_name) %>%
-      rename(created_at = info_created, updated = info_updated) %>%
-      rename(http_response_time_second = response_time_seconds)
-  })
-
-  output$note_text <- renderUI({
-    note_info <- "The endpoints queried by Lantern are limited to Fast Healthcare Interoperability
-      Resources (FHIR) endpoints published publicly by Certified API Developers in conformance
-      with the ONC Cures Act Final Rule, or discovered through the National Plan and Provider
-      Enumeration System (NPPES). This data, therefore, may not represent all FHIR endpoints
-      in existence. Insights gathered from this data should be framed accordingly."
-    res <- paste("<div style='font-size: 18px;'><b>Note:</b>", note_info, "</div>")
-    HTML(res)
+            )
   })
 
 }
