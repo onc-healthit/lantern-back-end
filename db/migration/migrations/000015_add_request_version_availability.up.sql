a8dafad041efeedc70512fbdd572cdb16f4b8538
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2021-09-10 12:18:05 -0400

    Fix syntax for adding default value to column

--- db/migration/migrations/000015_add_request_version_availability.up.sql
+++ db/migration/migrations/000015_add_request_version_availability.up.sql
@@ -1,8 +1,12 @@
 BEGIN;
 
-ALTER TABLE fhir_endpoints_metadata ADD COLUMN requested_fhir_version VARCHAR(500) SET DEFAULT 'None';
+ALTER TABLE fhir_endpoints_metadata ADD COLUMN requested_fhir_version VARCHAR(500);
 
-ALTER TABLE fhir_endpoints_availability ADD COLUMN requested_fhir_version VARCHAR(500) SET DEFAULT 'None';
+ALTER TABLE fhir_endpoints_availability ADD COLUMN requested_fhir_version VARCHAR(500);
+
+ALTER TABLE fhir_endpoints_metadata ALTER COLUMN requested_fhir_version SET DEFAULT 'None';
+
+ALTER TABLE fhir_endpoints_availability ALTER COLUMN requested_fhir_version SET DEFAULT 'None';
 
 CREATE OR REPLACE FUNCTION update_fhir_endpoint_availability_info() RETURNS TRIGGER AS $fhir_endpoints_availability$
     DECLARE
