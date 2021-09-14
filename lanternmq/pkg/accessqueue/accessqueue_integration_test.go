6f7a8eda5c3cbbfe87292c6c7540e63e598a9734
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2021-09-14 15:55:47 -0400

    Check what the message on the queue is when it is acknowledged

--- lanternmq/pkg/accessqueue/accessqueue_integration_test.go
+++ lanternmq/pkg/accessqueue/accessqueue_integration_test.go
@@ -170,12 +170,12 @@ func Test_QueueCount(t *testing.T) {
 	err = aq.SendToQueue(ctx, "queue count message", mq, &ch, qName)
 	th.Assert(t, err == nil, err)
 	// ack the message
-	_, deliveryOk, err := channel.Get(qName, true)
+	msg, deliveryOk, err := channel.Get(qName, true)
 	th.Assert(t, err == nil, err)
 
 	count, err = aq.QueueCount(qName, channel)
 	th.Assert(t, err == nil, err)
-	th.Assert(t, count == 1, fmt.Sprintf("there should be one message in the queue, instead there are %d. The delivery bool was %v", count, deliveryOk))
+	th.Assert(t, count == 1, fmt.Sprintf("there should be one message in the queue, instead there are %d. The delivery bool was %v. The message was %v", count, deliveryOk, msg))
 }
 
 func queueIsEmpty(t *testing.T, queueName string) {
