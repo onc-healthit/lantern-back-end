d16fcfb1276a09097f8eb87f8d9b92aaa5bd01dd
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2021-09-16 12:59:46 -0400

    Try shortening sleep time

--- lanternmq/pkg/accessqueue/accessqueue_integration_test.go
+++ lanternmq/pkg/accessqueue/accessqueue_integration_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/onc-healthit/lantern-back-end/lanternmq"
 	aq "github.com/onc-healthit/lantern-back-end/lanternmq/pkg/accessqueue"
 	"github.com/onc-healthit/lantern-back-end/lanternmq/rabbitmq"
+	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 	"github.com/streadway/amqp"
 )
@@ -48,6 +49,100 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
+func Test_ConnectToServerAndQueue(t *testing.T) {
+	queueIsEmpty(t, qName)
+	defer checkCleanQueue(t, qName, channel)
+
+	var chID lanternmq.ChannelID
+	var err error
+
+	// bad host
+	_, _, err = aq.ConnectToServerAndQueue(qUser, qPassword, "asdf", qPort, qName)
+	th.Assert(t, errors.Cause(err).Error() == "unable to connect to message queue", "expected error due to bad host")
+
+	// bad port
+	_, _, err = aq.ConnectToServerAndQueue(qUser, qPassword, qHost, "8080", qName)
+	th.Assert(t, errors.Cause(err).Error() == "unable to connect to message queue", "expected error due to bad port")
+
+	// bad username
+	_, _, err = aq.ConnectToServerAndQueue("nottheuser", qPassword, qHost, qPort, qName)
+	th.Assert(t, errors.Cause(err).Error() == "unable to connect to message queue", "expected error due to bad username")
+
+	// bad password
+	_, _, err = aq.ConnectToServerAndQueue(qUser, "notthepassword", qHost, qPort, qName)
+	th.Assert(t, errors.Cause(err).Error() == "unable to connect to message queue", "expected error due to bad password")
+
+	// bad queue name
+	_, _, err = aq.ConnectToServerAndQueue(qUser, qPassword, qHost, qPort, "notthequeuename")
+	th.Assert(t, errors.Cause(err).Error() == "queue notthequeuename does not exist", "expected error due to bad queue name")
+
+	// all ok
+	mq_, chID, err := aq.ConnectToServerAndQueue(qUser, qPassword, qHost, qPort, qName)
+	mq = &mq_
+	th.Assert(t, err == nil, err)
+	th.Assert(t, mq != nil, "expected message queue to be created")
+	th.Assert(t, chID != nil, "expected channel ID to be created")
+}
+
+func Test_ConnectToQueue(t *testing.T) {
+	queueIsEmpty(t, qName)
+	defer checkCleanQueue(t, qName, channel)
+
+	var err error
+
+	// set up
+
+	mq2 := &rabbitmq.MessageQueue{}
+	err = mq2.Connect(qUser, qPassword, qHost, qPort)
+	th.Assert(t, err == nil, "unable to connect to message queue server")
+	ch, err := mq2.CreateChannel()
+	th.Assert(t, err == nil, "unable to create channel to message queue server")
+
+	// all ok
+	mq_, _, err := aq.ConnectToQueue(mq2, ch, qName)
+	mq = &mq_
+	th.Assert(t, err == nil, err)
+
+	// queue does not exist
+	_, _, err = aq.ConnectToQueue(mq2, ch, "nonsense")
+	th.Assert(t, errors.Cause(err).Error() == "queue nonsense does not exist", err)
+
+	// channel does not exist
+	_, _, err = aq.ConnectToQueue(mq2, "ch", qName)
+	th.Assert(t, errors.Cause(err).Error() == "ChannelID not of correct type", "given channel should not exist")
+}
+
+func Test_CleanQueue(t *testing.T) {
+	queueIsEmpty(t, qName)
+	defer checkCleanQueue(t, qName, channel)
+
+	var err error
+
+	// set up
+	mq2 := &rabbitmq.MessageQueue{}
+	err = mq2.Connect(qUser, qPassword, qHost, qPort)
+	th.Assert(t, err == nil, "unable to connect to message queue server")
+	ch, err := mq2.CreateChannel()
+	th.Assert(t, err == nil, "unable to create channel to message queue server")
+
+	// all ok
+	mq_, _, err := aq.ConnectToQueue(mq2, ch, qName)
+	mq = &mq_
+	th.Assert(t, err == nil, err)
+
+	// add message to queue then clean
+	ctx := context.Background()
+	err = aq.SendToQueue(ctx, "clean queue message", mq, &ch, qName)
+	th.Assert(t, err == nil, err)
+
+	// ack the message so it will get purged
+	_, _, err = channel.Get(qName, true)
+	th.Assert(t, err == nil, err)
+
+	err = aq.CleanQueue(qName, channel)
+	th.Assert(t, err == nil, err)
+}
+
 
 func Test_QueueCount(t *testing.T) {
 	queueIsEmpty(t, qName)
@@ -77,15 +172,11 @@ func Test_QueueCount(t *testing.T) {
 	err = aq.SendToQueue(ctx, "queue count message", mq, &ch, qName)
 	th.Assert(t, err == nil, err)
 
-	time.Sleep(time.Duration(60) * time.Second)
-
-	/* ack the message
-	delivery, deliveryBool, err := channel.Get(qName, true)
-	th.Assert(t, err == nil, err)*/
+	time.Sleep(20 * time.Second)
 
 	count, err = aq.QueueCount(qName, channel)
 	th.Assert(t, err == nil, err)
-	th.Assert(t, count == 1, fmt.Sprintf("There are %d messages in the queue.", count))
+	th.Assert(t, count == 1, fmt.Sprintf("there should be one message in the queue, instead there are %d", count))
 
 	// ack the message
 	_, _, err = channel.Get(qName, true)
