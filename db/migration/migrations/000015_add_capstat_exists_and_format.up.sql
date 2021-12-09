082423b2b3542f78ecf309a4d1945a9133a4527c
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2021-12-09 11:01:57 -0500

    Add capstat_exists and capstat format to endpoint export table and create migrations for this change

---
+++ db/migration/migrations/000015_add_capstat_exists_and_format.up.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+DROP VIEW IF EXISTS endpoint_export;
+
+CREATE or REPLACE VIEW endpoint_export AS
+SELECT endpts.url, endpts.list_source, endpts.organization_names AS endpoint_names,
+    vendors.name as vendor_name,
+    endpts_info.tls_version, endpts_info.mime_types, endpts_metadata.http_response,
+    endpts_metadata.response_time_seconds, endpts_metadata.smart_http_response, endpts_metadata.errors,
+    EXISTS (SELECT 1 FROM fhir_endpoints_info WHERE capability_statement != 'null' AND endpts.url = fhir_endpoints_info.url) as CAP_STAT_EXISTS,
+    endpts_info.capability_fhir_version AS FHIR_VERSION,
+    endpts_info.capability_statement->>'publisher' AS PUBLISHER,
+    endpts_info.capability_statement->'software'->'name' AS SOFTWARE_NAME,
+    endpts_info.capability_statement->'software'->'version' AS SOFTWARE_VERSION,
+    endpts_info.capability_statement->'software'->'releaseDate' AS SOFTWARE_RELEASEDATE,
+    endpts_info.capability_statement->'format' AS FORMAT,
+    endpts_info.updated_at AS INFO_UPDATED, endpts_info.created_at AS INFO_CREATED,
+    endpts_info.requested_fhir_version,
+    orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME,
+    orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE,
+    links.confidence AS MATCH_SCORE, endpts_metadata.availability
+FROM endpoint_organization AS links
+RIGHT JOIN fhir_endpoints AS endpts ON links.url = endpts.url
+LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
+LEFT JOIN fhir_endpoints_metadata AS endpts_metadata ON endpts_info.metadata_id = endpts_metadata.id
+LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id
+LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id;
+
+COMMIT;
