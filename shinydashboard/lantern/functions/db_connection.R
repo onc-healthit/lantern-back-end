0a039b6f7d0c964da13af166f861414e620b3f89
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2021-12-18 10:37:16 -0500

    Add valid_fhr_versions to db_connection.R since it is run before the global variables are set

--- shinydashboard/lantern/functions/db_connection.R
+++ shinydashboard/lantern/functions/db_connection.R
@@ -37,6 +37,8 @@ db_tables <- list(
   vendors                     = tbl(db_connection, "vendors")
 )
 
+valid_fhir_versions <- c("0.4.0", "0.5.0", "1.0.0", "1.0.1", "1.0.2", "1.1.0", "1.2.0", "1.4.0", "1.6.0", "1.8.0", "3.0.0", "3.0.1", "3.0.2", "3.2.0", "3.3.0", "3.5.0", "3.5a.0", "4.0.0", "4.0.1")
+
 # Get the Endpoint export table and clean up for UI
 endpoint_export_tbl <- db_tables$endpoint_export %>%
   collect() %>%
