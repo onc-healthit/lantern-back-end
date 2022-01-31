0e11086b2621713a80a4b4cf700617412b933e13
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-01-31 14:36:25 -0500

    Change default value to null string in migration

--- db/migration/migrations/000015_add_supported_profiles.up.sql
+++ db/migration/migrations/000015_add_supported_profiles.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE fhir_endpoints_info ADD COLUMN IF NOT EXISTS supported_profiles JSONB;
-ALTER TABLE fhir_endpoints_info_history ADD COLUMN IF NOT EXISTS supported_profiles JSONB;
+ALTER TABLE fhir_endpoints_info ADD COLUMN IF NOT EXISTS supported_profiles JSONB DEFAULT 'null';
+ALTER TABLE fhir_endpoints_info_history ADD COLUMN IF NOT EXISTS supported_profiles JSONB DEFAULT 'null';
