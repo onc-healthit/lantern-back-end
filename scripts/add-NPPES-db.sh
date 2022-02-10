01513bf0398c607e36619e5a2f44d7af8f5b07b6
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-02-10 16:23:08 -0500

    Change NPPES resource updater so that it adds the resources directly to the database and then deletes the files

---
+++ scripts/add-NPPES-db.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+echo "Getting NPPES contact (endpoint) pfile into db"
+cd cmd/nppescontactpopulator
+go run main.go /etc/lantern/resources/endpoint_pfile.csv
+cd ..
+
+echo "Getting NPPES org pfile data into db"
+cd nppesorgpopulator
+go run main.go /etc/lantern/resources/npidata_pfile.csv
+
+echo "Running endpoint linker"
+cd ../endpointlinker
+go run main.go
