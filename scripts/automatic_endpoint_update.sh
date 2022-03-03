1ad4fee137e7c9fec0207f309bae69868a586d58
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-03-03 17:14:23 -0500

    Fix automatic update script to not include NPPES info

--- scripts/automatic_endpoint_update.sh
+++ scripts/automatic_endpoint_update.sh
@@ -4,4 +4,4 @@ SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 chmod +rx query-endpoint-resources.sh; ./query-endpoint-resources.sh
-chmod +rx populatedb_prod.sh; ./populatedb_prod.sh
+exec docker exec -it lantern-back-end_endpoint_manager_1 /etc/lantern/populatedb.sh
