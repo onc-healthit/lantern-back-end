28736aae70a3647c49e740e7f21da3f9350a9bf2
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-03-07 09:27:42 -0500

    Fix CHPL endpoint querier fileName code and create populatedb script with no NPPES or CHPL product info

--- scripts/automatic_endpoint_update.sh
+++ scripts/automatic_endpoint_update.sh
@@ -4,4 +4,4 @@ SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 chmod +rx query-endpoint-resources.sh; ./query-endpoint-resources.sh
-exec docker exec -it lantern-back-end_endpoint_manager_1 /etc/lantern/populatedb.sh
+exec docker exec -it lantern-back-end_endpoint_manager_1 /etc/lantern/populateEndpoints.sh
---
+++ scripts/populateEndpoints.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+# get endpoint data
+cd cmd/endpointpopulator
+
+jq -c '.[]' /etc/lantern/resources/EndpointResourcesList.json | while read endpoint; do
+    NAME=$(echo $endpoint | jq -c -r '.EndpointName')
+    FORMAT=$(echo $endpoint | jq -c -r '.FormatType')
+    FILENAME=$(echo $endpoint | jq -c -r '.FileName')
+    LISTURL=$(echo $endpoint | jq -c -r '.URL')
+
+    go run main.go /etc/lantern/resources/$FILENAME $FORMAT $NAME $LISTURL
+done
+
+# Only use the line below that populates the database with CareEvolution for development
+# go run main.go /etc/lantern/resources/CareEvolutionEndpointSources.json CareEvolution
+
+cd ..
+
+# run data validation to ensure number of endpoints does not exceed maximum for query interval
+cd datavalidation
+go run main.go
+cd ..
