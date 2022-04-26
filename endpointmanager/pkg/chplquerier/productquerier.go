4c2eb8fbbe8e8c6d5ea1f4eb0f5a6ccfbd2bb30f
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-04-26 10:46:31 -0400

    Add persistProduct change where CHPLID is checked first and then name and version to grab existing product

--- endpointmanager/pkg/chplquerier/productquerier.go
+++ endpointmanager/pkg/chplquerier/productquerier.go
@@ -241,7 +241,12 @@ func persistProduct(ctx context.Context,
 	if err != nil {
 		return err
 	}
-	existingDbProd, err := store.GetHealthITProductUsingNameAndVersion(ctx, newDbProd.Name, newDbProd.Version)
+
+	existingDbProd, err := store.GetHealthITProductUsingCHPLID(ctx, newDbProd.CHPLID)
+
+	if err == sql.ErrNoRows {
+		existingDbProd, err = store.GetHealthITProductUsingNameAndVersion(ctx, newDbProd.Name, newDbProd.Version)
+	}
 
 	newElement := true
 	if err == sql.ErrNoRows { // need to add new entry
