674817f816126a4e76c2a60ba6fb5c650e0ccb5f
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-04-28 16:31:04 -0400

    Implement Organizations Page (#272)
    * Add initial code for adding organizations page to dashboard
    
    * Add NPI ID to endpoint export table
    
    * Write functions for grabbing table with npi organization matches and endpoint list organization matches
    
    * Create organizations module page
    
    * Add confidence filter to organization page
    
    * Remove organization filter and add confidence filter
    
    * Fix migrations to include changes from previous migrations

--- db/migration/migrations/000018_add_npi_id.up.sql
+++ db/migration/migrations/000018_add_npi_id.up.sql
@@ -1,29 +1,24 @@
 BEGIN;
 
 DROP VIEW IF EXISTS endpoint_export;
-DROP INDEX IF EXISTS smart_capabilities_idx;
-
-ALTER TABLE fhir_endpoints_info ALTER COLUMN capability_statement TYPE JSONB;
-ALTER TABLE fhir_endpoints_info ALTER COLUMN smart_response TYPE JSONB;
-ALTER TABLE fhir_endpoints_info_history ALTER COLUMN capability_statement TYPE JSONB;
-ALTER TABLE fhir_endpoints_info_history ALTER COLUMN smart_response TYPE JSONB;
 
 CREATE or REPLACE VIEW endpoint_export AS
 SELECT endpts.url, endpts.list_source, endpts.organization_names AS endpoint_names,
     vendors.name as vendor_name,
     endpts_info.tls_version, endpts_info.mime_types, endpts_metadata.http_response,
     endpts_metadata.response_time_seconds, endpts_metadata.smart_http_response, endpts_metadata.errors,
-    EXISTS (SELECT 1 FROM fhir_endpoints_info WHERE capability_statement != 'null' AND endpts.url = fhir_endpoints_info.url) as CAP_STAT_EXISTS,
+    EXISTS (SELECT 1 FROM fhir_endpoints_info WHERE capability_statement::jsonb != 'null' AND endpts.url = fhir_endpoints_info.url) as CAP_STAT_EXISTS,
     endpts_info.capability_fhir_version AS FHIR_VERSION,
     endpts_info.capability_statement->>'publisher' AS PUBLISHER,
     endpts_info.capability_statement->'software'->'name' AS SOFTWARE_NAME,
     endpts_info.capability_statement->'software'->'version' AS SOFTWARE_VERSION,
     endpts_info.capability_statement->'software'->'releaseDate' AS SOFTWARE_RELEASEDATE,
     endpts_info.capability_statement->'format' AS FORMAT,
+    endpts_info.capability_statement->>'kind' AS KIND,
     endpts_info.updated_at AS INFO_UPDATED, endpts_info.created_at AS INFO_CREATED,
     endpts_info.requested_fhir_version,
     orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME,
-    orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE,
+    orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE, orgs.npi_id as NPI_ID,
     links.confidence AS MATCH_SCORE, endpts_metadata.availability
 FROM endpoint_organization AS links
 RIGHT JOIN fhir_endpoints AS endpts ON links.url = endpts.url
@@ -31,7 +26,5 @@ LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
 LEFT JOIN fhir_endpoints_metadata AS endpts_metadata ON endpts_info.metadata_id = endpts_metadata.id
 LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id
 LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id;
-
-CREATE INDEX smart_capabilities_idx ON fhir_endpoints_info ((smart_response->'capabilities'));
 
 COMMIT;
