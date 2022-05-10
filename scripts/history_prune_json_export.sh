17661329d50e8dff91c2d06b9148fdcea559355d
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-05-10 11:30:40 -0400

    Fix history_prune_json_export script to call history pruning function using docker exec

--- scripts/history_prune_json_export.sh
+++ scripts/history_prune_json_export.sh
@@ -3,8 +3,7 @@
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-cd ../endpointmanager/cmd/historypruning
-go run main.go true
+docker exec -it --workdir /go/src/app/cmd/historypruning lantern-back-end_endpoint_manager_1 go run main.go true
 
 cd ../../..
 
