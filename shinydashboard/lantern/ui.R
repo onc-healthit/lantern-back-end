70131634d9a16b1bed9b3e4a33965d97be453105
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-06-03 16:03:56 -0400

    Dashboard Accessibility (#282)
    * CSS changes for color and UI style elements of dashboard
    
    * Change disclaimer HTML to have an id that can be adjusted with CSS
    
    * Add aria-hidden tag to all icons, make footer a footer element, add script to add main role to content wrapper and change div in sidebar list to a li element, change info button text to black

--- shinydashboard/lantern/ui.R
+++ shinydashboard/lantern/ui.R
@@ -1,6 +1,5 @@
 # Define base user interface
 ui <- dashboardPage(
-
   dashboardHeader(
     title = "Lantern Dashboard",
     titleWidth = 200,
@@ -17,29 +16,73 @@ ui <- dashboardPage(
   # Sidebar with menu items for each module
   dashboardSidebar(
     sidebarMenu(id = "side_menu",
-      menuItem("Dashboard", tabName = "dashboard_tab", icon = icon("dashboard")),
-      menuItem("Endpoints", tabName = "endpoints_tab", icon = icon("table")),
-      menuItem("Organizations", tabName = "organizations_tab", icon = icon("hospital")),
-      menuItem("Resources", icon = icon("list-alt"), tabName = "resource_tab"),
-      menuItem("Implementation Guides", tabName = "implementation_tab", icon = icon("list-alt")),
-      menuItem("CapabilityStatement / Conformance Fields", icon = icon("list-alt"), tabName = "fields_tab"),
-      menuItem("CapabilityStatement / Conformance Profiles", icon = icon("list-alt"), tabName = "profile_tab", badgeLabel = "new"),
-      menuItem("Values", icon = icon("table"), tabName = "values_tab"),
-      menuItem("Performance", icon = icon("bar-chart-o"), tabName = "performance_tab"),
-      menuItem("CapabilityStatement / Conformance Size", icon = icon("hdd-o"), tabName = "capabilitystatementsize_tab"),
-      menuItem("Validations", icon = icon("clipboard-check"), tabName = "validations_tab", badgeColor = "green"),
-      menuItem("Security", icon = icon("id-card-o"), tabName = "security_tab"),
-      menuItem("SMART Response", icon = icon("list"), tabName = "smartresponse_tab"),
-      menuItem("Location", tabName = "location_tab", icon = icon("map")),
-      menuItem("Downloads", tabName = "downloads_tab", icon = icon("download")),
-      menuItem("About Lantern", tabName = "about_tab", icon = icon("info-circle")),
-      style = "white-space: normal",
-      hr()
+      menuItem("Dashboard", tabName = "dashboard_tab", icon = tags$i(class = "fa fa-dashboard", "aria-hidden" = "true", role = "presentation", "aria-label" = "dashboard icon")),
+      menuItem("Endpoints", tabName = "endpoints_tab", icon = tags$i(class = "fa fa-table", "aria-hidden" = "true", role = "presentation", "aria-label" = "table icon")),
+      menuItem("Organizations", tabName = "organizations_tab", icon = tags$i(class = "fa fa-hospital", "aria-hidden" = "true", role = "presentation", "aria-label" = "hospital icon"), badgeLabel = "new", badgeColor = "green"),
+      menuItem("Resources", icon = tags$i(class = "fa fa-list-alt", "aria-hidden" = "true", role = "presentation", "aria-label" = "list-alt icon"), tabName = "resource_tab"),
+      menuItem("Implementation Guides", tabName = "implementation_tab", icon = tags$i(class = "fa fa-list-alt", "aria-hidden" = "true", role = "presentation", "aria-label" = "list-alt icon")),
+      menuItem("CapabilityStatement / Conformance Fields", icon = tags$i(class = "fa fa-list-alt", "aria-hidden" = "true", role = "presentation", "aria-label" = "list-alt icon"), tabName = "fields_tab"),
+      menuItem("CapabilityStatement / Conformance Profiles", icon = tags$i(class = "fa fa-list-alt", "aria-hidden" = "true", role = "presentation", "aria-label" = "list-alt icon"), tabName = "profile_tab"),
+      menuItem("Values", icon = tags$i(class = "fa fa-table", "aria-hidden" = "true", role = "presentation", "aria-label" = "table icon"), tabName = "values_tab"),
+      menuItem("Performance", icon = tags$i(class = "fa fa-bar-chart-o", "aria-hidden" = "true", role = "presentation", "aria-label" = "bar-chart-o icon"), tabName = "performance_tab"),
+      menuItem("CapabilityStatement / Conformance Size", icon = tags$i(class = "fa fa-hdd-o", "aria-hidden" = "true", role = "presentation", "aria-label" = "hdd-o icon"), tabName = "capabilitystatementsize_tab"),
+      menuItem("Validations", icon = tags$i(class = "fa fa-clipboard-check", "aria-hidden" = "true", role = "presentation", "aria-label" = "clipboard-check icon"), tabName = "validations_tab"),
+      menuItem("Security", icon = tags$i(class = "fa fa-id-card-o", "aria-hidden" = "true", role = "presentation", "aria-label" = "id-card-o icon"), tabName = "security_tab"),
+      menuItem("SMART Response", icon = tags$i(class = "fa fa-list", "aria-hidden" = "true", role = "presentation", "aria-label" = "list icon"), tabName = "smartresponse_tab"),
+      menuItem("Location", tabName = "location_tab", icon = tags$i(class = "fa fa-map", "aria-hidden" = "true", role = "presentation", "aria-label" = "map icon")),
+      menuItem("Downloads", tabName = "downloads_tab", icon = tags$i(class = "fa fa-download", "aria-hidden" = "true", role = "presentation", "aria-label" = "download icon")),
+      menuItem("About Lantern", tabName = "about_tab", icon = tags$i(class = "fa fa-info-circle", "aria-hidden" = "true", role = "presentation", "aria-label" = "info-circle icon")),
+      style = "white-space: normal"
     )
   ),
 
   # Set up contents for each menu item (tab) in the sidebar
   dashboardBody(
+    tags$head(tags$style(HTML("
+      .skin-blue .main-header .navbar {
+        background-color: #1B5A7F;
+      }
+      .skin-blue .main-header .logo {
+        background-color: #1B5A7F;
+      }
+      .small-box {
+        color: black!important;
+      }
+      .badge {
+        color: black!important;
+      }
+      .NA {
+        color: #696464!important;
+      }
+      .lantern-url {
+        color: #0044FF!important;
+        text-decoration: underline;
+      }
+      .small-box p {
+        font-size: 20px;
+      }
+      .sidebar-menu {
+         border-bottom: 1px solid white;
+      }
+      #dashboard_page-show_info {
+        color: black;
+      }
+    "))),
+    tags$script(
+      "let elems = document.getElementsByClassName('content-wrapper');
+        elems[0].setAttribute('role', 'main');
+
+        var e = document.getElementById('side_menu');
+
+        var d = document.createElement('li');
+        d.classList.add('sidebarMenuSelectedTabItem', 'shiny-bound-input')
+        d.dataset.value = e.dataset.value;
+
+        e.parentNode.replaceChild(d, e);
+        e.remove();
+        d.id = 'side_menu'
+        "
+    ),
     tags$head(tags$link(rel = "shortcut icon", href = "images/favicon.ico")),
     development_banner(devbanner),
     uiOutput("resource_tab_popup"),
@@ -99,11 +142,16 @@ ui <- dashboardPage(
       tabItem("about_tab",
               img(src = "images/lantern-logo@1x.png", width = "300px"),
               br(),
-              includeHTML("about-lantern.html"),
-              p("For information about the data sources, algorithms, and query intervals used by Lantern, please see the",
+              div(
+                class = "footer",
+                includeHTML("about-lantern.html"),
+                p("For information about the data sources, algorithms, and query intervals used by Lantern, please see the",
                 a("documentation available here.", href = "Lantern_Data_Sources_And_Algorithms.pdf", target = "_blank"))
               )
+        )
     ),
-    uiOutput("htmlFooter")
+    tags$footer(class = "footer",
+      includeHTML("disclaimer.html")
+    )
   )
 )
