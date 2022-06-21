a795530c1563af72aabeff2f2597cb03ab5776a5
Jacob Daniel Lane <JLANE@MITRE.ORG>
2022-06-21 15:57:28 -0400

    Web scraper for new canvas endpoint list

---
+++ endpointmanager/pkg/chplendpointquerier/canvaswebscraper.go
@@ -0,0 +1,71 @@
+package chplendpointquerier
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/chromedp/chromedp"
+)
+
+func Canvaswebscraper(CHPLURL string, fileToWriteTo string) {
+
+	var lanternEntryList []LanternEntry
+	var endpointEntryList EndpointList
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	var htmlContent string
+
+	// Chromedp will wait for webpage to run javascript code to generate api search results before grapping HTML
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(CHPLURL),
+		chromedp.OuterHTML("html", &htmlContent, chromedp.ByQuery),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("table").Each(func(index int, tablehtml *goquery.Selection) {
+		if index == 1 {
+			tablehtml.Find("tbody").Each(func(indextr int, rowhtml *goquery.Selection) {
+				rowhtml.Find("tr").Each(func(indextr int, rowbodyhtml *goquery.Selection) {
+					var entry LanternEntry
+					tableEntries := rowbodyhtml.Find("td")
+					if tableEntries.Length() > 0 {
+						organizationName := strings.TrimSpace(tableEntries.Eq(0).Text())
+						URL := strings.TrimSpace(tableEntries.Eq(1).Text())
+
+						entry.OrganizationName = organizationName
+						entry.URL = URL
+
+						lanternEntryList = append(lanternEntryList, entry)
+					}
+				})
+			})
+		}
+	})
+
+	endpointEntryList.Endpoints = lanternEntryList
+
+	finalFormatJSON, err := json.MarshalIndent(endpointEntryList, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("../../../resources/prod_resources/"+fileToWriteTo, finalFormatJSON, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
