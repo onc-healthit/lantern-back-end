bd215eaf22e0b8b23fcc65ec99defbca98ae2749
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-07-18 12:45:03 -0400

    Write 3 endpoint webscrapers- not working must fix

---
+++ endpointmanager/pkg/endpointwebscraper/endpointwebscraper.go
@@ -0,0 +1,35 @@
+package endpointwebscraper
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+type EndpointList struct {
+	Endpoints []LanternEntry `json:"Endpoints"`
+}
+
+type LanternEntry struct {
+	URL              string `json:"URL"`
+	OrganizationName string `json:"OrganizationName"`
+	NPIID            string `json:"NPIID"`
+}
+
+var oneUpURL = "https://1up.health/fhir-endpoint-directory"
+var careEvolutionURL = "https://fhir.docs.careevolution.com/overview/public_endpoints.html"
+var athenaHealthURL = "https://mydata.athenahealth.com/aserver"
+var techCareURL = "https://devportal.techcareehr.com/Serviceurls"
+var carefluenceURL = "https://carefluence.com/carefluence-fhir-endpoints/"
+
+func EndpointListWebscraper(vendorURL string, vendor string, fileToWriteTo string) {
+
+	if vendorURL == oneUpURL || vendorURL == careEvolutionURL {
+		HTMLtablewebscraper(vendorURL, vendor, fileToWriteTo)
+	} else if vendorURL == athenaHealthURL {
+		log.Info("Going to run athena webscraper")
+		Athenawebscraper(vendorURL, fileToWriteTo)
+	} else if vendorURL == techCareURL {
+		Techcarewebscraper(vendorURL, fileToWriteTo)
+	} else if vendorURL == carefluenceURL {
+		Carefluenceebscraper(vendorURL, fileToWriteTo)
+	}
+}
