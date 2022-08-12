5c75ed155391e782c2a28f7bfdf0ac671dc2d861
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-08-12 15:43:44 -0400

    Change name of NextGen webscraper from CHPLwebscraper to NextGenwebscraper

--- endpointmanager/pkg/chplendpointquerier/NextGenwebscraper.go
+++ endpointmanager/pkg/chplendpointquerier/NextGenwebscraper.go
@@ -12,7 +12,7 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
-func CHPLwebscraper(CHPLURL string, fileToWriteTo string) {
+func NextGenwebscraper(CHPLURL string, fileToWriteTo string) {
 
 	var lanternEntryList []LanternEntry
 	var endpointEntryList EndpointList
