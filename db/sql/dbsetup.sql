8a1c60b18ea4921cead32552c5a68ffc7abcd879
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-08-30 13:41:50 -0400

    Take organization location information out of endpoint export view and create new organization location view

--- db/sql/dbsetup.sql
+++ db/sql/dbsetup.sql
@@ -340,16 +340,23 @@ SELECT endpts.url, endpts.list_source, endpts.organization_names AS endpoint_nam
     endpts_info.capability_statement->'format' AS FORMAT,
     endpts_info.capability_statement->>'kind' AS KIND,
     endpts_info.updated_at AS INFO_UPDATED, endpts_info.created_at AS INFO_CREATED,
-    endpts_info.requested_fhir_version,
-    orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME,
+    endpts_info.requested_fhir_version, endpts_metadata.availability
+FROM fhir_endpoints AS endpts
+LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
+LEFT JOIN fhir_endpoints_fmetadata AS endpts_metadata ON endpts_info.metadata_id = endpts_metadata.id
+LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id;
+
+CREATE or REPLACE VIEW organization_location AS
+    SELECT endpts.url, endpts.organization_names AS endpoint_names, endpts_info.capability_fhir_version AS FHIR_VERSION, 
+    endpts_info.requested_fhir_version, vendors.name as vendor_name, orgs.name AS ORGANIZATION_NAME, orgs.secondary_name AS ORGANIZATION_SECONDARY_NAME,
     orgs.taxonomy, orgs.Location->>'state' AS STATE, orgs.Location->>'zipcode' AS ZIPCODE, orgs.npi_id as NPI_ID,
-    links.confidence AS MATCH_SCORE, endpts_metadata.availability
+    links.confidence AS MATCH_SCORE
 FROM endpoint_organization AS links
 RIGHT JOIN fhir_endpoints AS endpts ON links.url = endpts.url
-LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url
-LEFT JOIN fhir_endpoints_metadata AS endpts_metadata ON endpts_info.metadata_id = endpts_metadata.id
+LEFT JOIN fhir_endpoints_info AS endpts_info ON endpts.url = endpts_info.url   
 LEFT JOIN vendors ON endpts_info.vendor_id = vendors.id
-LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id;
+LEFT JOIN npi_organizations AS orgs ON links.organization_npi_id = orgs.npi_id
+WHERE match_score > .97 AND orgs.Location->>'zipcode' IS NOT null;
 
 CREATE INDEX fhir_endpoints_url_idx ON fhir_endpoints (url);
 CREATE INDEX fhir_endpoints_info_url_idx ON fhir_endpoints_info (url);
