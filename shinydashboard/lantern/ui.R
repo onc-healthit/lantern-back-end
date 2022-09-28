f47949c9c935dd5c7c6baaa6feefbd6cd1cf6a35
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-09-28 10:14:00 -0400

    Add aria-label to all dropdown buttons to explain how to use them for accessibility purposes

--- shinydashboard/lantern/ui.R
+++ shinydashboard/lantern/ui.R
@@ -510,6 +510,13 @@ ui <- dashboardPage(
               dropDownButton.setAttribute('aria-label', 'Dropdown filter menu button. Press the down arrow key to open the filter menu, use the tab or arrow keys to navigate through options, press enter to select a filter option, and use the escape key to close the filter menu.')
             }  
           }
+
+          if (mutation.target.id === \"show_filters\") {
+            let dropDownButtons = document.getElementsByClassName(\"dropdown-toggle\")
+            for (let dropDownButton of dropDownButtons) {
+              dropDownButton.setAttribute('aria-label', 'Dropdown filter menu button. Press the down arrow key to open the filter menu, use the tab or arrow keys to navigate through options, press enter to select a filter option, and use the escape key to close the filter menu.')
+            }  
+          }     
         }
       })
 
