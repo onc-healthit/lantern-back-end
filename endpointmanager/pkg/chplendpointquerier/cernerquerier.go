9f4320b6c3f0d8dc4326647baf35b7a15b902f06
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-10-13 15:05:29 -0400

    Fix go linter errors

--- endpointmanager/pkg/chplendpointquerier/cernerquerier.go
+++ endpointmanager/pkg/chplendpointquerier/cernerquerier.go
@@ -1,16 +1,16 @@
 package chplendpointquerier
 
 import (
-	"strings"
 	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/helpers"
 	log "github.com/sirupsen/logrus"
+	"strings"
 )
 
 func CernerQuerier(chplURL string, fileToWriteTo string) {
 
 	chplURL = strings.ReplaceAll(chplURL, "github.com", "raw.githubusercontent.com")
 	chplURL = strings.Replace(chplURL, "/blob", "", 1)
-	
+
 	var endpointEntryList EndpointList
 
 	respBody, err := helpers.QueryEndpointList(chplURL)
