4b2b389a18b3b66b366bc649c665216ddd9047b4
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-10-21 14:50:40 -0400

    Remove athena parser download csv function

--- endpointmanager/pkg/chplendpointquerier/athenaparser.go
+++ endpointmanager/pkg/chplendpointquerier/athenaparser.go
@@ -2,7 +2,6 @@ package chplendpointquerier
 
 import (
 	"io"
-	"net/http"
 	"strings"
 
 	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/helpers"
@@ -55,26 +54,4 @@ func AthenaCSVParser(CHPLURL string, fileToWriteTo string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-// DownloadFile will download a url to a local file. It's efficient because it will
-// write as it downloads and not load the whole file into memory.
-func DownloadFile(filepath string, url string) error {
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
 }
