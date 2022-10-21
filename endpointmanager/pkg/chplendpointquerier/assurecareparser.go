beb4b746ea121efb99547180e661cb528a7bb421
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-10-21 15:09:06 -0400

    Create parsers and add resource files for Abeo Solutions, AssureCare, and Bizmatics

---
+++ endpointmanager/pkg/chplendpointquerier/assurecareparser.go
@@ -0,0 +1,57 @@
+package chplendpointquerier
+
+import (
+	"io"
+	"strings"
+
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/helpers"
+
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func AssureCareCSVParser(CHPLURL string, fileToWriteTo string) {
+	var lanternEntryList []LanternEntry
+	var endpointEntryList EndpointList
+
+	csvFilePath := "./fhir-base-urls.csv"
+
+	csvReader, file, err := helpers.QueryAndOpenCSV(CHPLURL, csvFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	for {
+		rec, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var entry LanternEntry
+
+		organizationName := ""
+		URL := strings.TrimSpace(rec[1])
+
+		entry.OrganizationName = organizationName
+		entry.URL = URL
+
+		lanternEntryList = append(lanternEntryList, entry)
+	}
+
+	endpointEntryList.Endpoints = lanternEntryList
+
+	err = WriteCHPLFile(endpointEntryList, fileToWriteTo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.Remove(csvFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
