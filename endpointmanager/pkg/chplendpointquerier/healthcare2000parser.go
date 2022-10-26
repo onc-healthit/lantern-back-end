f39a977787a9bf732334deefe45ad2b79676960d
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-10-26 14:11:35 -0400

    Add webparsers for healthcare2000, healthsamurai, and triarq sources and add endpoint lists to dev and prod folders

---
+++ endpointmanager/pkg/chplendpointquerier/healthcare2000parser.go
@@ -0,0 +1,50 @@
+package chplendpointquerier
+
+import (
+	"strings"
+
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/helpers"
+
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func HealthCare2000SVParser(CHPLURL string, fileToWriteTo string) {
+	var lanternEntryList []LanternEntry
+	var endpointEntryList EndpointList
+
+	csvFilePath := "./MDVitaFHIRUrls.csv"
+
+	csvReader, file, err := helpers.QueryAndOpenCSV(CHPLURL, csvFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Only want the first URL in the csv file
+	rec, err := csvReader.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var entry LanternEntry
+
+	URL := strings.TrimSpace(rec[1])
+
+	entry.URL = URL
+
+	lanternEntryList = append(lanternEntryList, entry)
+
+	endpointEntryList.Endpoints = lanternEntryList
+
+	err = WriteCHPLFile(endpointEntryList, fileToWriteTo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.Remove(csvFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
