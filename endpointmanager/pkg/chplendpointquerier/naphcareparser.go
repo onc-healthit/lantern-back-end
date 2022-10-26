8c0aeea460c155fb900d298b5c56652d4210791d
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-10-26 15:20:34 -0400

    Add Napchare parser that does not work right now due to MITRE security

---
+++ endpointmanager/pkg/chplendpointquerier/naphcareparser.go
@@ -0,0 +1,45 @@
+package chplendpointquerier
+
+import (
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/helpers"
+	log "github.com/sirupsen/logrus"
+)
+
+func NaphCareWebscraper(CHPLURL string, fileToWriteTo string) {
+
+	var lanternEntryList []LanternEntry
+	var endpointEntryList EndpointList
+
+	doc, err := helpers.ChromedpQueryEndpointList(CHPLURL, "#page-header")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".container").Each(func(index int, bodyElems *goquery.Selection) {
+		bodyElems.Find(".WordSection1").Each(func(index int, wordSectionElems *goquery.Selection) {
+			bodyTextElems := wordSectionElems.Find(".BodyText")
+			productionEndpointElem := bodyTextElems.First()
+			endpointURLElem := productionEndpointElem.Find("a")
+			hrefText, exists := endpointURLElem.Attr("href")
+			if exists {
+				var entry LanternEntry
+
+				entryURL := strings.TrimSpace(hrefText)
+				entry.URL = entryURL
+
+				lanternEntryList = append(lanternEntryList, entry)
+			}
+		})
+	})
+
+	endpointEntryList.Endpoints = lanternEntryList
+
+	err = WriteCHPLFile(endpointEntryList, fileToWriteTo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
