af2c10ed819c0ea029c633ebee82f8c51bb3f03d
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-11-09 14:35:42 -0500

    Add bundle querier parser file to repo

---
+++ endpointmanager/pkg/chplendpointquerier/bundleQuerierParser.go
@@ -0,0 +1,24 @@
+package chplendpointquerier
+
+import (
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/helpers"
+	log "github.com/sirupsen/logrus"
+)
+
+func BundleQuerierParser(CHPLURL string, fileToWriteTo string) {
+
+	var endpointEntryList EndpointList
+
+	respBody, err := helpers.QueryEndpointList(CHPLURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// convert bundle data to lantern format
+	endpointEntryList.Endpoints = BundleToLanternFormat(respBody)
+
+	err = WriteCHPLFile(endpointEntryList, fileToWriteTo)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
