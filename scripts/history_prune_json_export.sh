92bb6c90b14958f07242af3bd9ce18a7969487d3
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-12-01 15:25:28 -0500

    Remove -it flag from history_prune_json_export docker exec command

--- scripts/history_prune_json_export.sh
+++ scripts/history_prune_json_export.sh
@@ -3,7 +3,7 @@
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-docker exec -it --workdir /go/src/app/cmd/historypruning lantern-back-end_endpoint_manager_1 go run main.go true
+docker exec --workdir /go/src/app/cmd/historypruning lantern-back-end_endpoint_manager_1 go run main.go true
 
 cd ../../..
 
