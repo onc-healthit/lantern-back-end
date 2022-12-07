14167a05a699921aed619e61d94c6af5d44a2455
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-12-07 14:53:36 -0500

    Fix CHPL report query so percentages are calculated correctly

--- endpointmanager/cmd/CHPLreport/main.go
+++ endpointmanager/cmd/CHPLreport/main.go
@@ -23,20 +23,20 @@ func main() {
 	// get report data and export to tmp/export.csv
 	getCHPLEndpointsStatement := `
 	COPY (
-        SELECT DISTINCT
+		SELECT DISTINCT
    			f.list_source AS service_base_url,
    			f.url as endpoint,
 			vendors.name as developer,
    			CASE WHEN COUNT(metadata.http_response)=0
         		THEN NULL
-        		ELSE ROUND(COUNT(NULLIF(metadata.http_response, 200))::numeric/COUNT(metadata.http_response) * 100,2)
+        		ELSE ROUND(COUNT(NULLIF(metadata.http_response, 200))::numeric/totals.metadata_total * 100,2)
    			END AS server_status_percent,
    			string_agg(DISTINCT metadata.http_response::varchar, ',') AS server_status,
    			CASE WHEN hist.capability_statement = 'null'
         		THEN 'missing'
         		ELSE 'valid'
    			END AS cap_stat_status,
-   			ROUND(COUNT(hist.capability_statement = 'null')::numeric / COUNT(hist.capability_statement) * 100,2) AS cap_stat_status_percent,
+   			ROUND(COUNT(hist.capability_statement = 'null')::numeric / totals.cap_stat_total * 100,2) AS cap_stat_status_percent,
 			CASE WHEN metadata.http_response != 200
 				THEN 'Unreachable'
 			    ELSE 'Doesn''t service Capability Statement request'
@@ -51,13 +51,18 @@ func main() {
 		) AS hist ON f.url = hist.url
 		LEFT JOIN vendors ON hist.vendor_id = vendors.id
 		LEFT JOIN fhir_endpoints_metadata AS metadata ON hist.metadata_id = metadata.id
+		LEFT JOIN (SELECT f.url, COUNT(f.capability_statement::jsonb) as cap_stat_total, COUNT(m.http_response) as metadata_total 
+				   FROM fhir_endpoints_info_history f, fhir_endpoints_metadata m WHERE f.metadata_id = m.id AND age(f.updated_at) < '30 days'
+				   GROUP BY f.url) as totals ON totals.url = hist.url
 		WHERE list_source_info.is_chpl = true AND age(hist.updated_at) < '30 days'
 		GROUP BY
 			f.list_source,
 			f.url,
 			hist.capability_statement,
 			metadata.http_response,
-			vendors.name
+			vendors.name,
+			totals.metadata_total,
+			totals.cap_stat_total
 		HAVING hist.capability_statement = 'null' OR metadata.http_response != 200
 	)
 	TO '/tmp/export.csv'
