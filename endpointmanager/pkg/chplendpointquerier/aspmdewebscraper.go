8431a864c36beea0e707e322dcada16670ab8e5a
archita-ekkirala <archita.ekkirala@mettles.com>
2024-10-31 16:21:25 -0500

    LANTERN-757: Fixing go lintr

--- endpointmanager/pkg/chplendpointquerier/aspmdewebscraper.go
+++ endpointmanager/pkg/chplendpointquerier/aspmdewebscraper.go
@@ -1,39 +1,38 @@
 package chplendpointquerier
 
 import (
-    "github.com/PuerkitoBio/goquery"
-    "github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/helpers"
-    log "github.com/sirupsen/logrus"
-    "strings"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/helpers"
+	log "github.com/sirupsen/logrus"
+	"strings"
 )
 
 func AspMDeWebscraper(chplURL string, fileToWriteTo string) {
-    found := false
+	found := false
 
-    baseURL := strings.TrimSuffix(chplURL, "/fhir_aspmd.asp#apiendpoints")
+	baseURL := strings.TrimSuffix(chplURL, "/fhir_aspmd.asp#apiendpoints")
 	baseURL = strings.TrimSuffix(baseURL, "/fhir_aspmd.asp")
 
+	doc, err := helpers.ChromedpQueryEndpointList(chplURL, "p")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    doc, err := helpers.ChromedpQueryEndpointList(chplURL, "p")
-    if err != nil {
-        log.Fatal(err)
-    }
+	doc.Find("h1").Each(func(i int, s *goquery.Selection) {
+		sectionTitle := s.Text()
 
-    doc.Find("h1").Each(func(i int, s *goquery.Selection) {
-        sectionTitle := s.Text()
-
-        if sectionTitle == "Public Endpoint" {
-            s.Parent().Find("a").Each(func(j int, link *goquery.Selection) {
-                if found {
-                    return
-                }
-                href, exists := link.Attr("href")
-                if exists && strings.Contains(href, "endpoints.asp") {
-                    bundleURL := baseURL + "/" + href
-                    found = true
+		if sectionTitle == "Public Endpoint" {
+			s.Parent().Find("a").Each(func(j int, link *goquery.Selection) {
+				if found {
+					return
+				}
+				href, exists := link.Attr("href")
+				if exists && strings.Contains(href, "endpoints.asp") {
+					bundleURL := baseURL + "/" + href
+					found = true
 					BundleQuerierParser(bundleURL, fileToWriteTo)
-                }
-            })
-        }
-    })
+				}
+			})
+		}
+	})
 }
