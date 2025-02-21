0254f05924de96d1ff6fae8f04b30ace508a7948
daedalus-s <sreenikethaathreya@gmail.com>
2025-02-20 23:17:02 -0500

    History table only gets inserted with a new row if a new endpoint gets added

---
+++ db/migration/migrations/000031_update_history_trigger.down.sql
@@ -0,0 +1,32 @@
+-- LANTERN-825: Update the history trigger to only insert a new row when there are changes
+BEGIN;
+
+-- Drop the new trigger and function
+DROP TRIGGER IF EXISTS add_fhir_endpoint_info_history_trigger ON fhir_endpoints_info CASCADE;
+DROP FUNCTION IF EXISTS add_fhir_endpoint_info_history() CASCADE;
+
+-- Restore original function
+CREATE OR REPLACE FUNCTION add_fhir_endpoint_info_history() RETURNS TRIGGER AS $fhir_endpoints_info_history$
+    BEGIN
+        --
+        -- Create a row in fhir_endpoints_info_history to reflect the operation performed on fhir_endpoints_info,
+        -- make use of the special variable TG_OP to work out the operation.
+        --
+        IF (TG_OP = 'DELETE') THEN
+            INSERT INTO fhir_endpoints_info_history SELECT 'D', now(), user, OLD.*;
+            RETURN OLD;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            INSERT INTO fhir_endpoints_info_history SELECT 'U', now(), user, NEW.*;
+            RETURN NEW;
+        ELSIF (TG_OP = 'INSERT') THEN
+            INSERT INTO fhir_endpoints_info_history SELECT 'I', now(), user, NEW.*;
+            RETURN NEW;
+        END IF;
+        RETURN NULL; -- result is ignored since this is an AFTER trigger
+    END;
+$fhir_endpoints_info_history$ LANGUAGE plpgsql;
+
+-- Restore original trigger
+CREATE TRIGGER add_fhir_endpoint_info_history_trigger AFTER INSERT OR UPDATE OR DELETE ON fhir_endpoints_info FOR EACH ROW EXECUTE FUNCTION add_fhir_endpoint_info_history();
+
+COMMIT;
