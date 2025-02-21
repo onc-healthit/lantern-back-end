8c5a5e59d332995ab146c14b9b9555e19442c3f5
daedalus-s <sreenikethaathreya@gmail.com>
2025-02-20 23:38:27 -0500

    Updated history trigger to only add new entries to history table

---
+++ db/migration/migrations/000031_update_history_trigger.up.sql
@@ -0,0 +1,13 @@
+-- LANTERN-825: Update the history trigger to only insert a new row if the data has changed
+BEGIN;
+
+-- Drop existing trigger and function
+DROP TRIGGER IF EXISTS add_fhir_endpoint_info_history_trigger ON fhir_endpoints_info CASCADE;
+DROP FUNCTION IF EXISTS add_fhir_endpoint_info_history() CASCADE;
+
+CREATE OR REPLACE FUNCTION add_fhir_endpoint_info_history() RETURNS TRIGGER AS $$ BEGIN IF (TG_OP = 'UPDATE' AND NEW.id IS NOT DISTINCT FROM OLD.id AND NEW.healthit_mapping_id IS NOT DISTINCT FROM OLD.healthit_mapping_id AND NEW.vendor_id IS NOT DISTINCT FROM OLD.vendor_id AND NEW.url IS NOT DISTINCT FROM OLD.url AND NEW.tls_version IS NOT DISTINCT FROM OLD.tls_version AND NEW.mime_types IS NOT DISTINCT FROM OLD.mime_types AND NEW.capability_statement::text IS NOT DISTINCT FROM OLD.capability_statement::text AND NEW.validation_result_id IS NOT DISTINCT FROM OLD.validation_result_id AND NEW.included_fields::text IS NOT DISTINCT FROM OLD.included_fields::text AND NEW.operation_resource::text IS NOT DISTINCT FROM OLD.operation_resource::text AND NEW.supported_profiles::text IS NOT DISTINCT FROM OLD.supported_profiles::text AND NEW.created_at IS NOT DISTINCT FROM OLD.created_at AND NEW.updated_at IS NOT DISTINCT FROM OLD.updated_at AND NEW.smart_response::text IS NOT DISTINCT FROM OLD.smart_response::text AND NEW.requested_fhir_version IS NOT DISTINCT FROM OLD.requested_fhir_version AND NEW.capability_fhir_version IS NOT DISTINCT FROM OLD.capability_fhir_version) THEN RETURN NEW; END IF; IF (TG_OP = 'DELETE') THEN INSERT INTO fhir_endpoints_info_history SELECT 'D', now(), user, OLD.*; RETURN OLD; ELSIF (TG_OP = 'UPDATE') THEN INSERT INTO fhir_endpoints_info_history SELECT 'U', now(), user, NEW.*; RETURN NEW; ELSIF (TG_OP = 'INSERT') THEN INSERT INTO fhir_endpoints_info_history SELECT 'I', now(), user, NEW.*; RETURN NEW; END IF; RETURN NULL; END; $$ LANGUAGE plpgsql;
+
+-- Create new trigger
+CREATE TRIGGER add_fhir_endpoint_info_history_trigger AFTER INSERT OR UPDATE OR DELETE ON fhir_endpoints_info FOR EACH ROW EXECUTE FUNCTION add_fhir_endpoint_info_history();
+
+COMMIT;
--- db/sql/dbsetup.sql
+++ db/sql/dbsetup.sql
@@ -480,3 +480,11 @@ CREATE INDEX healthit_products_certification_status_idx ON healthit_products (ce
 CREATE INDEX healthit_products_chpl_id_idx ON healthit_products (chpl_id);
 CREATE INDEX fhir_endpoint_organizations_map_id_idx ON fhir_endpoint_organizations_map (id);
 CREATE INDEX fhir_endpoint_organizations_map_org_database_id_idx ON fhir_endpoint_organizations_map (org_database_id);
+
+-- LANTERN-825: Add history trigger and function
+DROP TRIGGER IF EXISTS add_fhir_endpoint_info_history_trigger ON fhir_endpoints_info CASCADE;
+DROP FUNCTION IF EXISTS add_fhir_endpoint_info_history() CASCADE;
+
+CREATE OR REPLACE FUNCTION add_fhir_endpoint_info_history() RETURNS TRIGGER AS $$ BEGIN IF (TG_OP = 'UPDATE' AND NEW.id IS NOT DISTINCT FROM OLD.id AND NEW.healthit_mapping_id IS NOT DISTINCT FROM OLD.healthit_mapping_id AND NEW.vendor_id IS NOT DISTINCT FROM OLD.vendor_id AND NEW.url IS NOT DISTINCT FROM OLD.url AND NEW.tls_version IS NOT DISTINCT FROM OLD.tls_version AND NEW.mime_types IS NOT DISTINCT FROM OLD.mime_types AND NEW.capability_statement::text IS NOT DISTINCT FROM OLD.capability_statement::text AND NEW.validation_result_id IS NOT DISTINCT FROM OLD.validation_result_id AND NEW.included_fields::text IS NOT DISTINCT FROM OLD.included_fields::text AND NEW.operation_resource::text IS NOT DISTINCT FROM OLD.operation_resource::text AND NEW.supported_profiles::text IS NOT DISTINCT FROM OLD.supported_profiles::text AND NEW.created_at IS NOT DISTINCT FROM OLD.created_at AND NEW.updated_at IS NOT DISTINCT FROM OLD.updated_at AND NEW.smart_response::text IS NOT DISTINCT FROM OLD.smart_response::text AND NEW.requested_fhir_version IS NOT DISTINCT FROM OLD.requested_fhir_version AND NEW.capability_fhir_version IS NOT DISTINCT FROM OLD.capability_fhir_version) THEN RETURN NEW; END IF; IF (TG_OP = 'DELETE') THEN INSERT INTO fhir_endpoints_info_history SELECT 'D', now(), user, OLD.*; RETURN OLD; ELSIF (TG_OP = 'UPDATE') THEN INSERT INTO fhir_endpoints_info_history SELECT 'U', now(), user, NEW.*; RETURN NEW; ELSIF (TG_OP = 'INSERT') THEN INSERT INTO fhir_endpoints_info_history SELECT 'I', now(), user, NEW.*; RETURN NEW; END IF; RETURN NULL; END; $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER add_fhir_endpoint_info_history_trigger AFTER INSERT OR UPDATE OR DELETE ON fhir_endpoints_info FOR EACH ROW EXECUTE FUNCTION add_fhir_endpoint_info_history();
