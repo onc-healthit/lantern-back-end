e789c29bb4d60a86fd6cda9fc870c70a5098b9bb
rishi-salunkhe-mettle <rishi.salunkhe@mettles.com>
2025-02-24 13:16:12 -0600

    Corrected the docker container name in the refresh materialized views script.

--- scripts/refresh_materialized_views.sh
+++ scripts/refresh_materialized_views.sh
@@ -5,11 +5,11 @@ current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
 
 echo "$current_datetime - Refreshing and reindexing Lantern materialized views." >> $log_file
 
-docker exec -t lantern-back-end-postgres-1 psql -t -c "REFRESH MATERIALIZED VIEW CONCURRENTLY mv_http_responses;" -U lantern -d lantern || {
+docker exec -t lantern-back-end_postgres_1 psql -t -c "REFRESH MATERIALIZED VIEW CONCURRENTLY mv_http_responses;" -U lantern -d lantern || {
     echo "$current_datetime - Lantern failed to refresh mv_http_responses." >> $log_file
 }
 
-docker exec -t lantern-back-end-postgres-1 psql -t -c "REINDEX INDEX CONCURRENTLY mv_http_responses_uniq;" -U lantern -d lantern || {
+docker exec -t lantern-back-end_postgres_1 psql -t -c "REINDEX INDEX CONCURRENTLY mv_http_responses_uniq;" -U lantern -d lantern || {
     echo "$current_datetime - Lantern failed to reindex mv_http_responses_uniq." >> $log_file
 }
 
