ae99ec92adb23f72390d1284d72b92b9692dfdbb
rishi-salunkhe-mettle <rishi.salunkhe@mettles.com>
2025-02-28 12:54:47 -0600

    Fixed the path to the logs directory for the refresh script for dashboard MVs

--- scripts/refresh_materialized_views.sh
+++ scripts/refresh_materialized_views.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-log_file="/etc/lantern/refresh_materialized_views_logs.txt"
-current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
+log_file="/etc/lantern/logs/refresh_materialized_views_logs.txt"
 
-echo "$current_datetime - Refreshing and reindexing Lantern materialized views." >> $log_file
+echo "$(date +"%Y-%m-%d %H:%M:%S") - Refreshing and reindexing Lantern materialized views." >> $log_file
 
 docker exec -t lantern-back-end_postgres_1 psql -t -c "REFRESH MATERIALIZED VIEW CONCURRENTLY mv_http_responses;" -U lantern -d lantern || {
-    echo "$current_datetime - Lantern failed to refresh mv_http_responses." >> $log_file
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Lantern failed to refresh mv_http_responses." >> $log_file
 }
 
 docker exec -t lantern-back-end_postgres_1 psql -t -c "DROP INDEX IF EXISTS mv_http_responses_uniq;" -U lantern -d lantern || {
-    echo "$current_datetime - Lantern failed to drop mv_http_responses_uniq." >> $log_file
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Lantern failed to drop mv_http_responses_uniq." >> $log_file
 }
 
 docker exec -t lantern-back-end_postgres_1 psql -t -c "CREATE UNIQUE INDEX mv_http_responses_uniq ON mv_http_responses (aggregation_date, vendor_name, http_code);" -U lantern -d lantern || {
-    echo "$current_datetime - Lantern failed to create mv_http_responses_uniq." >> $log_file
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Lantern failed to create mv_http_responses_uniq." >> $log_file
 }
 
-echo "$current_datetime - done." >> $log_file
+echo "$(date +"%Y-%m-%d %H:%M:%S") - done." >> $log_file
