881e9ca5fa5c783d202bac133bd4f91214fc7c3f
jash-mettles <jash.shah@mettles.com>
2025-04-10 00:30:04 -0500

    fixed download button on the endpoints tab

--- shinydashboard/lantern/modules/endpointsmodule.R
+++ shinydashboard/lantern/modules/endpointsmodule.R
@@ -176,7 +176,7 @@ selected_fhir_endpoints <- reactive({
   # Create the format for the csv
   csv_format <- reactive({
     res <- selected_fhir_endpoints() %>%
-      select(-label, -status, -availability, -fhir_version, -urlModal, -condensed_endpoint_names) %>%
+      select(-status, -availability, -fhir_version, -urlModal, -condensed_endpoint_names) %>%
       mutate(endpoint_names = ifelse(length(strsplit(endpoint_names, ";")[[1]]) > 100, paste0("Subset of Organizations, see Lantern Website for full list:", paste0(head(strsplit(endpoint_names, ";")[[1]], 100), collapse = ";")), endpoint_names)) %>%
       rename(api_information_source_name = endpoint_names, certified_api_developer_name = vendor_name) %>%
       rename(created_at = info_created, updated = info_updated) %>%
