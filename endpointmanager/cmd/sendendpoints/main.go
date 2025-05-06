de04f8baabc38ddc9d0f470ef9d6424657a02843
jash-mettles <jash.shah@mettles.com>
2025-05-06 15:18:03 -0500

    endpoint manager analysis changes

--- endpointmanager/cmd/sendendpoints/main.go
+++ endpointmanager/cmd/sendendpoints/main.go
@@ -2,46 +2,176 @@ package main
 
 import (
 	"context"
-	"sync"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"runtime/pprof"
+	"syscall"
+	"time"
+
+	"net/http"
+	_ "net/http/pprof" // Import pprof handlers
 
 	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/config"
 	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/endpointmanager/postgresql"
 	"github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/helpers"
-	se "github.com/onc-healthit/lantern-back-end/endpointmanager/pkg/sendendpoints"
-	"github.com/onc-healthit/lantern-back-end/lanternmq/pkg/accessqueue"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
+var (
+	memprofile  = flag.String("memprofile", "", "write memory profile to `file`")
+	cpuprofile  = flag.String("cpuprofile", "", "write cpu profile to `file`")
+	httpProfile = flag.String("httpprofile", "", "enable http profiling on specified address, e.g., ':6060'")
+	profileFreq = flag.Duration("profilefreq", 60*time.Minute, "frequency to collect memory profiles")
+)
+
 func main() {
-	err := config.SetupConfig()
-	helpers.FailOnError("", err)
+	flag.Parse()
 
-	store, err := postgresql.NewStore(viper.GetString("dbhost"), viper.GetInt("dbport"), viper.GetString("dbuser"), viper.GetString("dbpassword"), viper.GetString("dbname"), viper.GetString("dbsslmode"))
-	helpers.FailOnError("", err)
-	log.Info("Successfully connected to DB!")
-
-	// Set up the queue for sending messages to capabilityquerier
-	capQName := viper.GetString("versionsquery_qname")
-	mq, channelID, err := accessqueue.ConnectToServerAndQueue(viper.GetString("quser"), viper.GetString("qpassword"), viper.GetString("qhost"), viper.GetString("qport"), capQName)
-	helpers.FailOnError("", err)
-	log.Info("Successfully connected to capabilityquerier Queue!")
-
-	errs := make(chan error)
-
-	// Infinite query loop
-	var wg sync.WaitGroup
-	ctx := context.Background()
-	wg.Add(1)
-	capInterval := viper.GetInt("capquery_qryintvl")
-	go se.GetEnptsAndSend(ctx, &wg, capQName, capInterval, store, &mq, &channelID, errs)
-
-	wg.Add(2)
-	go se.HistoryPruning(ctx, &wg, capInterval, store, errs)
-
-	for elem := range errs {
-		log.Warn(elem)
+	// Set up CPU profiling if requested
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatalf("Could not create CPU profile: %v", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatalf("Could not start CPU profile: %v", err)
+		}
+		defer pprof.StopCPUProfile()
 	}
 
-	wg.Wait()
+	// Set up HTTP profiling server if requested
+	if *httpProfile != "" {
+		go func() {
+			log.Printf("Starting pprof HTTP server on %s", *httpProfile)
+			log.Println(http.ListenAndServe(*httpProfile, nil))
+		}()
+	}
+
+	// Set up periodic memory profiling if requested
+	if *memprofile != "" && *profileFreq > 0 {
+		go func() {
+			for {
+				time.Sleep(*profileFreq)
+				writeMemProfile(*memprofile)
+			}
+		}()
+	}
+
+	// Original main functionality
+	var err error
+
+	err = config.SetupConfig()
+	helpers.FailOnError("", err)
+
+	store, err := postgresql.NewStore(
+		viper.GetString("dbhost"),
+		viper.GetInt("dbport"),
+		viper.GetString("dbuser"),
+		viper.GetString("dbpassword"),
+		viper.GetString("dbname"),
+		viper.GetString("dbsslmode"),
+	)
+	helpers.FailOnError("", err)
+	defer store.Close()
+
+	// Set up signal handler for graceful shutdown
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		select {
+		case <-signalChan:
+			log.Println("Received shutdown signal, stopping...")
+			// Capture a memory profile on shutdown
+			if *memprofile != "" {
+				writeMemProfile(fmt.Sprintf("%s.shutdown", *memprofile))
+			}
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+
+	// Configure logging
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetLevel(logrus.InfoLevel)
+
+	for {
+		// Force garbage collection before starting a new cycle
+		runtime.GC()
+
+		// Show memory stats before starting
+		var m runtime.MemStats
+		runtime.ReadMemStats(&m)
+		logrus.WithFields(logrus.Fields{
+			"alloc_mb":       m.Alloc / 1024 / 1024,
+			"total_alloc_mb": m.TotalAlloc / 1024 / 1024,
+			"sys_mb":         m.Sys / 1024 / 1024,
+			"num_gc":         m.NumGC,
+		}).Info("Memory stats before endpoint processing")
+
+		// Run the endpoint manager
+		err = sendendpoints.SendFHIREndpointsToQueue(ctx, store)
+		if err != nil {
+			if ctx.Err() != nil {
+				// Context cancelled, shutting down
+				break
+			}
+			logrus.WithError(err).Error("Error sending FHIR endpoints to queue")
+		}
+
+		// Show memory stats after completion
+		runtime.ReadMemStats(&m)
+		logrus.WithFields(logrus.Fields{
+			"alloc_mb":       m.Alloc / 1024 / 1024,
+			"total_alloc_mb": m.TotalAlloc / 1024 / 1024,
+			"sys_mb":         m.Sys / 1024 / 1024,
+			"num_gc":         m.NumGC,
+		}).Info("Memory stats after endpoint processing")
+
+		// Capture memory profile after processing
+		if *memprofile != "" {
+			writeMemProfile(fmt.Sprintf("%s.%d", *memprofile, time.Now().Unix()))
+		}
+
+		// Get the query interval from configuration
+		queryInterval := viper.GetInt("capquery_qryintvl")
+		interval := time.Duration(queryInterval) * time.Minute
+		logrus.WithField("interval_minutes", queryInterval).Info("Waiting for next cycle")
+
+		// Wait for interval or shutdown signal
+		select {
+		case <-time.After(interval):
+			// Continue to next cycle
+		case <-ctx.Done():
+			// Shutdown requested
+			logrus.Info("Shutdown requested, exiting...")
+			return
+		}
+	}
+}
+
+func writeMemProfile(filename string) {
+	// Force garbage collection before profiling
+	runtime.GC()
+
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Printf("Could not create memory profile: %v", err)
+		return
+	}
+	defer f.Close()
+
+	log.Printf("Writing memory profile to %s", filename)
+	if err := pprof.WriteHeapProfile(f); err != nil {
+		log.Printf("Could not write memory profile: %v", err)
+	}
 }
