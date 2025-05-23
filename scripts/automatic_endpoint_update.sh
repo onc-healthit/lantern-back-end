b53048285d11ce144c911482fde2a7b97879c496
rishi-salunkhe-mettle <rishi.salunkhe@mettles.com>
2025-05-23 15:58:06 -0500

    Removed the docker cp commands from the population script since they are redundant. Removed the commented code from automatic_populatedb_prod script.

--- scripts/automatic_endpoint_update.sh
+++ scripts/automatic_endpoint_update.sh
@@ -10,16 +10,10 @@ current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
 
 chmod +rx query-endpoint-resources.sh; ./query-endpoint-resources.sh
 
-cd ../resources
-cp -r prod_resources resources
-docker cp resources lantern-back-end_endpoint_manager_1:/etc/lantern
-
 docker exec lantern-back-end_endpoint_manager_1 /etc/lantern/populateEndpoints.sh || {
     echo "$current_datetime - Lantern failed to save endpoint information in database after endpoint resource list updates." >> $log_file
     echo "Lantern failed to save endpoint information in database after endpoint resource list updates." | /usr/bin/mail -s "Automatic endpoint update and database population error." ${EMAIL}
     exit 0
 }
-
-rm -r resources
 
 echo "$current_datetime - Automatic Endpoint Update complete" >> $log_file
