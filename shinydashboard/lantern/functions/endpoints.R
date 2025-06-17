d08fbc033e42bddba8ee9c0eddc25c33d1f8fc01
rishi-salunkhe-mettle <rishi.salunkhe@mettles.com>
2025-06-17 17:33:35 -0500

    Implemented SQL-based pagination for the table in Resources tab

--- shinydashboard/lantern/functions/endpoints.R
+++ shinydashboard/lantern/functions/endpoints.R
@@ -229,8 +229,11 @@ get_vendor_list <- function(endpoint_export_tbl) {
 }
 
 # Return the endpoint counts for selected FHIR resources, operations, fhir version and vendor name
-get_fhir_resource_by_op <- function(db_connection, operations_vec, fhir_versions_vec, resource_types_vec, vendor_name) {
+get_fhir_resource_by_op <- function(db_connection, operations_vec, fhir_versions_vec, resource_types_vec, vendor_name, page_size = -1, offset = -1) {
   
+  message("PAGESIZE: ", page_size)
+  message("OFFSET: ", offset)
+
   # Create the base query string
   query_str <- "SELECT resource_type as type, fhir_version, SUM(endpoint_count) as n 
             FROM mv_resource_interactions
@@ -249,6 +252,10 @@ get_fhir_resource_by_op <- function(db_connection, operations_vec, fhir_versions
   
   query_str <- paste0(query_str, " GROUP BY (resource_type, fhir_version)
             ORDER BY resource_type")
+
+  if (page_size > -1 && offset > -1) {
+    query_str <- paste0(query_str, " LIMIT ", page_size, " OFFSET ", offset)
+  }
 
   query <- glue_sql(query_str, .con = db_connection)
 
