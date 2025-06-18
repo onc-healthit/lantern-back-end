a72e9ddac056658029ecd361ffb98dae1fbabe71
shrilekkala <shri.lekkala@gmail.com>
2025-06-18 10:36:58 -0500

    Remove search query input reset logic from organizations tab

--- shinydashboard/lantern/server.R
+++ shinydashboard/lantern/server.R
@@ -1411,12 +1411,3 @@ output$filter_profile_table <- DT::renderDataTable({
     class = 'stripe hover compact'
   )
 })
-
-# reset organizations module inputs when navigating again to organizations tab
-  observeEvent(input$side_menu, {
-      if (input$side_menu == "organizations_tab") { 
-        updateTextInput(session, "organizations_page-org_search_query", value = "")
-      }
-  }, ignoreInit = TRUE)
-
-}
