0ac05bb994e58916423e3da9ff4cc7be2b96f03c
shrilekkala <shri.lekkala@gmail.com>
2025-07-23 15:57:53 -0500

    Add organizations API handler with filter support
    - Added handler in restendpoints.R
    - Supports developer, fhir_version, identifier, and hti1 filters
    - Validates developer against CHPL vendor list and FHIR version against allowed set
    - Refactored get_organization_csv_data() to query from the new aggregated view
    - Included UI documentation and examples with guidance on filter usage and URL encoding

--- api/download/downloadsmodule.R
+++ api/download/downloadsmodule.R
@@ -55,3 +55,97 @@ download_data <- function(db_tables) {
     rename(http_response_time_second = response_time_seconds)
   #write.csv(csvdata, file, row.names = FALSE)
 }
+
+
+# Get organization data and transform to csv
+get_organization_csv_data <- function(db_connection, developer = NULL, fhir_versions = NULL, identifier = NULL, hti1 = NULL) {
+  query <- "
+    WITH base_data AS (
+      SELECT 
+        organization_name,
+        identifiers_csv as identifier,
+        addresses_csv as address,
+        org_urls_csv as org_url,
+        endpoint_urls_csv as url,
+        fhir_versions_array,
+        vendor_names_array
+      FROM mv_organizations_aggregated
+      WHERE TRUE"
+  
+  params <- list()
+
+  # Developer filter
+  if (!is.null(developer)) {
+    query <- paste0(query, " AND vendor_names_array && ARRAY[{developer*}]")
+    params$developer <- developer
+  }
+
+  # FHIR version filter
+  if (!is.null(fhir_versions)) {
+    query <- paste0(query, " AND fhir_versions_array && ARRAY[{fhir_versions*}]")
+    params$fhir_versions <- fhir_versions
+  }
+
+  # Identifier filter
+  if (!is.null(identifier)) {
+    query <- paste0(query, " AND identifiers_csv = {identifier_exact}")
+    params$identifier_exact <- paste0("NPI: ", identifier)
+  }
+
+  # HTI-1 filter
+  if (!is.null(hti1) && hti1 == "present") {
+    query <- paste0(query, " AND ((identifiers_csv IS NOT NULL AND identifiers_csv <> '')",
+                           " OR (addresses_csv IS NOT NULL AND addresses_csv <> ''))")
+  }
+
+  # Continue query with CROSS JOINs and filtering for output
+  query <- paste0(query, "
+    )
+    SELECT 
+      organization_name,
+      identifier,
+      address,
+      org_url,
+      url,
+      string_agg(DISTINCT fhir_version, E'\\n') AS fhir_version,
+      string_agg(DISTINCT vendor_name, E'\\n') AS vendor_name
+    FROM base_data bd
+    CROSS JOIN LATERAL unnest(bd.fhir_versions_array) AS fhir_version
+    CROSS JOIN LATERAL unnest(bd.vendor_names_array) AS vendor_name
+    WHERE 1=1")
+
+  # Re-apply developer and fhir_version filters to unnest output
+  if (!is.null(developer)) {
+    query <- paste0(query, " AND vendor_name = ANY(ARRAY[{developer_display*}])")
+    params$developer_display <- developer
+  }
+
+  if (!is.null(fhir_versions)) {
+    query <- paste0(query, " AND fhir_version = ANY(ARRAY[{fhir_versions_display*}])")
+    params$fhir_versions_display <- fhir_versions
+  }
+
+  # Finalize
+  query <- paste0(query, "
+    GROUP BY organization_name, identifier, address, org_url, url
+    ORDER BY organization_name")
+
+  # Build SQL safely
+  if (length(params) > 0) {
+    sql_query <- do.call(glue::glue_sql, c(list(query, .con = db_connection), params))
+  } else {
+    sql_query <- glue::glue_sql(query, .con = db_connection)
+  }
+
+  df <- DBI::dbGetQuery(db_connection, sql_query)
+
+  # Clean output
+  df <- df %>%
+    mutate(
+      org_url = ifelse(is.na(org_url) | org_url == "NA", "", org_url),
+      identifier = ifelse(is.na(identifier), "", identifier),
+      address = ifelse(is.na(address), "", address)
+    )
+
+  return(df)
+}
