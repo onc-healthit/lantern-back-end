63bea5ee2772cddfeb360435121cedd881450317
shrilekkala <shri.lekkala@gmail.com>
2025-09-09 14:41:51 -0500

    Update query interval for CHPL API

--- endpointmanager/pkg/chplquerier/chplquerier.go
+++ endpointmanager/pkg/chplquerier/chplquerier.go
@@ -48,7 +48,7 @@ func getJSON(ctx context.Context, client *http.Client, chplURL *url.URL, userAge
 	// request ceritified products list
 	// Adds a short delay between request
 	// LANTERN-721: Increased the delay to 1.2 seconds since the CHPL API rate limit is one request per second
-	time.Sleep(time.Duration(1200 * time.Millisecond))
+	time.Sleep(time.Duration(1500 * time.Millisecond))
 	req, err := http.NewRequest("GET", chplURL.String(), nil)
 	if err != nil {
 		return nil, errors.Wrap(err, "creating http request failed")
