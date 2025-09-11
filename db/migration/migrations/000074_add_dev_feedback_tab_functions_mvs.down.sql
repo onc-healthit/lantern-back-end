77d2465eda378da29edde7763488462a1b7643a6
jash-mettles <jash.shah@mettles.com>
2025-09-11 15:31:46 -0500

    Fixed down migration file to delete the functions and materialized views in order

--- db/migration/migrations/000074_add_dev_feedback_tab_functions_mvs.down.sql
+++ db/migration/migrations/000074_add_dev_feedback_tab_functions_mvs.down.sql
@@ -1,5 +1,11 @@
 BEGIN;
 
+DROP MATERIALIZED VIEW IF EXISTS mv_organization_quality CASCADE;
+
+DROP MATERIALIZED VIEW IF EXISTS mv_organization_quality_summary CASCADE;
+
+DROP MATERIALIZED VIEW IF EXISTS mv_organization_identifier_summary CASCADE;
+
 DROP FUNCTION IF EXISTS validate_npi_luhn(TEXT);
 
 DROP FUNCTION IF EXISTS is_address_like(TEXT);
@@ -9,11 +15,5 @@ DROP FUNCTION IF EXISTS validate_identifier_value(TEXT, TEXT);
 DROP FUNCTION IF EXISTS is_valid_organization_name(TEXT);
 
 DROP FUNCTION IF EXISTS is_valid_organization_address(TEXT);
-
-DROP MATERIALIZED VIEW IF EXISTS mv_organization_quality CASCADE;
-
-DROP MATERIALIZED VIEW IF EXISTS mv_organization_quality_summary CASCADE;
-
-DROP MATERIALIZED VIEW IF EXISTS mv_organization_identifier_summary CASCADE;
 
 COMMIT;
