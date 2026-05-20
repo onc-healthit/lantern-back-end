b6566bf3f2dd697de8fb429ab7e020bd5f11e6ef
rishi-salunkhe-mettle <rishi.salunkhe@mettles.com>
2026-05-20 15:43:15 -0500

    Added SQL composite indexes for the fhir_endpoints_info and fhir_endpoints_availability tables to speed up queries

--- db/sql/dbsetup.sql
+++ db/sql/dbsetup.sql
@@ -2980,3 +2980,15 @@ CREATE INDEX idx_mv_orgs_final_fhir_versions ON mv_organizations_final USING GIN
 CREATE INDEX idx_mv_orgs_final_vendor_names ON mv_organizations_final USING GIN(vendor_names_array);
 CREATE INDEX idx_mv_orgs_final_urls ON mv_organizations_final USING GIN(urls_array);
 CREATE INDEX idx_mv_orgs_final_is_chpl ON mv_organizations_final USING GIN(is_chpl_array);
+
+-- Composite index covering the most common query pattern:
+--   WHERE url = $1 AND requested_fhir_version = $2
+-- Previously only separate single-column indexes existed, forcing the query planner
+-- to pick one and filter by the other.
+CREATE INDEX idx_fhir_endpoints_info_url_reqver
+    ON fhir_endpoints_info (url, requested_fhir_version);
+
+-- Same composite for the availability table, used by the
+-- update_fhir_endpoint_availability_info trigger on every metadata insert/update.
+CREATE INDEX idx_fhir_endpoints_availability_url_reqver
+    ON fhir_endpoints_availability (url, requested_fhir_version);
