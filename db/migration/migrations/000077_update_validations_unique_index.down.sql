5cb31378c86d65dbd0846ec4b05f17d4902a21b6
shrilekkala <shri.lekkala@gmail.com>
2026-06-01 16:04:01 -0500

    Fix validation results plot MV refresh failure

---
+++ db/migration/migrations/000077_update_validations_unique_index.down.sql
@@ -0,0 +1,10 @@
+BEGIN;
+
+DROP INDEX IF EXISTS mv_validation_results_plot_unique_idx;
+CREATE UNIQUE INDEX mv_validation_results_plot_unique_idx 
+ON mv_validation_results_plot(url, fhir_version, vendor_name, rule_name, valid, expected, actual);
+
+DROP INDEX IF EXISTS mv_validation_failures_unique_idx;
+CREATE UNIQUE INDEX mv_validation_failures_unique_idx ON mv_validation_failures (url, fhir_version, vendor_name, rule_name);
+
+COMMIT;
