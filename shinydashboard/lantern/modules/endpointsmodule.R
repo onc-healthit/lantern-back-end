b918cc92814ad685a212b3b5d8a15cd8ece7d16a
rishi-salunkhe-mettle <rishi.salunkhe@mettles.com>
2026-06-05 16:25:43 -0500

    Renumbered the migration file for list_source grouping and added a new line for list_source values separator

--- shinydashboard/lantern/modules/endpointsmodule.R
+++ shinydashboard/lantern/modules/endpointsmodule.R
@@ -360,7 +360,7 @@ endpointsmodule <- function(
                     list_source)) %>%
       ungroup() %>%
       group_by(across(-list_source)) %>%
-      summarise(list_source = paste(unique(list_source), collapse = "; "), .groups = "drop") %>%
+      summarise(list_source = paste(unique(list_source), collapse = "\n"), .groups = "drop") %>%
       rename(api_information_source_name = endpoint_names, api_developer_name = vendor_name) %>%
       rename(created_at = info_created, updated = info_updated) %>%
       rename(http_response_time_second = response_time_seconds) %>%
